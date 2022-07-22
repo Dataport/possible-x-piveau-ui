e9644fee9b7bad1d0cd278530f7ffdb336057f38
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-07-22 13:54:01 +0200

    refactor: replaced log messages with error messages in scripts

--- scripts/ensure_module.js
+++ scripts/ensure_module.js
@@ -5,7 +5,7 @@ if ( ! fs.existsSync(target)) {
   if (process.platform !== "win32") { // Not operating under MS Windows
     fs.symlinkSync("../../src/modules/", target, "dir");
   } else {
-    console.log("Please either run localinstall or run link-module as admin or copy a symlink to the modules folder into node_modules\\@piveau\\piveau-hub-ui-modules.");
+    console.error("Please either run localinstall or run link-module as admin or copy a symlink to the modules folder into node_modules\\@piveau\\piveau-hub-ui-modules.");
   }
 }
 
