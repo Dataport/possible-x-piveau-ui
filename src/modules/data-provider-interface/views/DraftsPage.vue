512d43b0d37fc060a9976b9b6b87dd377bdd06a9
Duy Minh Vo <duy.minh.vo@fokus.fraunhofer.de>
2022-07-27 13:50:42 +0200

    feat(DraftsPage): refactor drafts list as table, add catalogue id column

--- src/modules/data-provider-interface/views/DraftsPage.vue
+++ src/modules/data-provider-interface/views/DraftsPage.vue
@@ -1,42 +1,53 @@
 <template>
   <div class="d-flex flex-column bg-transparent container-fluid justify-content-between mt-n5 content">
-    <h1 class="small-headline">My draft datasets</h1>
-    <ul class="list-group col-md-6 m-auto">
-      <li
-        v-for="{id, catalog} in getUserDrafts"
-        :key="`draft@${id}`"
-        class="list-group-item"
-        :data-cy="`draft@${id}`"
-      >
-        <span>{{ id }}</span>
-        <span class="float-right">
-          <button type="button" class="btn btn-secondary">
-            <app-link
-              class="dropdown-toggle text-nowrap text-decoration-none"
-              fragment="#" role="button" id="linkedDataDropdown"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false">
-              <span :title="$t('message.metadata.linkedData')"
-                    data-toggle="tooltip"
-                    data-placement="top">
-                     {{ $t('message.metadata.linkedData') }}
-              </span>
-            </app-link>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="linkedDataDropdown">
-              <app-link :to="createLinkedMetricsURL(id, catalog, 'rdf')" target="_blank" class="dropdown-item"><div class="px-2 py-1">RDF/XML</div></app-link>
-              <app-link :to="createLinkedMetricsURL(id, catalog, 'ttl')" target="_blank" class="dropdown-item"><div class="px-2 py-1">Turtle</div></app-link>
-              <app-link :to="createLinkedMetricsURL(id, catalog, 'n3')" target="_blank" class="dropdown-item"><div class="px-2 py-1">Notation3</div></app-link>
-              <app-link :to="createLinkedMetricsURL(id, catalog, 'nt')" target="_blank" class="dropdown-item"><div class="px-2 py-1">N-Triples</div></app-link>
-              <app-link :to="createLinkedMetricsURL(id, catalog, 'jsonld')" target="_blank" class="dropdown-item"><div class="px-2 py-1">JSON-LD</div></app-link>
-            </div>
-          </button>
-          <button type="button" class="btn btn-secondary" @click="handleEdit(id, catalog)">Edit</button>
-          <button type="button" class="btn btn-primary" @click="handleConfirmPublish(id, catalog)">Publish</button>
-          <button type="button" class="btn btn-danger" @click="handleConfirmDelete(id, catalog)">Delete</button>
-        </span>
-      </li>
-    </ul>
+    <h1 class="small-headline">Draft datasets</h1>
+    <div class="col-12 col-md-8 m-auto">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Dataset ID</th>
+            <th scope="col">Catalogue ID</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="{id, catalog} in getUserDrafts"
+            :key="`draft@${id}`"
+            :data-cy="`draft@${id}`"
+          >
+            <td>{{ id }}</td>
+            <td>{{ catalog }}</td>
+            <td>
+              <button type="button" class="btn btn-secondary">
+                <app-link
+                  class="dropdown-toggle text-nowrap text-decoration-none"
+                  fragment="#" role="button" id="linkedDataDropdown"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false">
+                  <span :title="$t('message.metadata.linkedData')"
+                        data-toggle="tooltip"
+                        data-placement="top">
+                        {{ $t('message.metadata.linkedData') }}
+                  </span>
+                </app-link>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="linkedDataDropdown">
+                  <app-link :to="createLinkedMetricsURL(id, catalog, 'rdf')" target="_blank" class="dropdown-item"><div class="px-2 py-1">RDF/XML</div></app-link>
+                  <app-link :to="createLinkedMetricsURL(id, catalog, 'ttl')" target="_blank" class="dropdown-item"><div class="px-2 py-1">Turtle</div></app-link>
+                  <app-link :to="createLinkedMetricsURL(id, catalog, 'n3')" target="_blank" class="dropdown-item"><div class="px-2 py-1">Notation3</div></app-link>
+                  <app-link :to="createLinkedMetricsURL(id, catalog, 'nt')" target="_blank" class="dropdown-item"><div class="px-2 py-1">N-Triples</div></app-link>
+                  <app-link :to="createLinkedMetricsURL(id, catalog, 'jsonld')" target="_blank" class="dropdown-item"><div class="px-2 py-1">JSON-LD</div></app-link>
+                </div>
+              </button>
+              <button type="button" class="btn btn-secondary" @click="handleEdit(id, catalog)">Edit</button>
+              <button type="button" class="btn btn-primary" @click="handleConfirmPublish(id, catalog)">Publish</button>
+              <button type="button" class="btn btn-danger" @click="handleConfirmDelete(id, catalog)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <app-confirmation-dialog id="draftsModal" confirm="Confirm" :loading="modalProps.loading" @confirm="modalProps.confirm">
       {{ modalProps.message }}
