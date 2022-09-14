d1e3def5b89517d97c4dfc3779b69ece924357bb
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-09-14 12:32:02 +0200

    refactor: created DatasetDetailsIsUsedBy.vue

--- src/modules/datasetDetails/DatasetDetailsDataset.vue
+++ src/modules/datasetDetails/DatasetDetailsDataset.vue
@@ -404,37 +404,12 @@
         </ul>
       </div>
     </div>
-    <!-- DATA IS_USED_BY_SECTION -->
-    <div class="mt-5" v-if="showObject(getExtendedMetadata)">
-      <div class="row">
-        <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
-              @click="toggleIsUsedBy()">
-          <span class="arrow text-dark"
-                v-if="!isUsedBy.isVisible">
-            <i class="material-icons">keyboard_arrow_down</i>
-          </span>
-          <span class="arrow text-dark" v-else>
-            <i class="material-icons">keyboard_arrow_up</i>
-          </span>
-        </div>
-        <div class="col-11 py-2 bg-white">
-          <h2 class="heading"
-              data-cy="documentation-toggle"
-              @click="toggleIsUsedBy()">{{ $t('message.datasetDetails.isUsedBy') }} ({{ getExtendedMetadata ? getExtendedMetadata.is_used_by.length.toLocaleString('fi') : 0 }})</h2>
-        </div>
-        <span v-if="isUsedBy.isVisible" class="list list-unstyled col-12">
-          <hr>
-            <ul class="sectionList" v-if="getExtendedMetadata.is_used_by.length != null || getExtendedMetadata.is_used_by != undefined ">
-                <li v-for="(link, i) in getExtendedMetadata.is_used_by" :key="i">
-                  <a :href="link" target="_blank">{{link}}</a>
-                </li>
-            </ul>
-          <hr>
-        </span>
-      </div>
-    </div>
 
-    <!-- DATA RELATED RESOURCE -->
+    <dataset-details-is-used-by
+      v-if="showObject(getExtendedMetadata)"
+      :getExtendedMetadata="getExtendedMetadata"
+    />
+
     <dataset-details-relations
       v-if="showArray(getRelations)"
       :getRelations="getRelations"
@@ -485,11 +460,16 @@
   import DatasetDetailsSubject from "@/modules/datasetDetails/DatasetDetailsSubject.vue";
   import DatasetDetailsInfo from "@/modules/datasetDetails/DatasetDetailsInfo.vue";
   import DatasetDetailsMap from "@/modules/datasetDetails/DatasetDetailsMap.vue";
+  import DatasetDetailsExtendedMetaData
+    from "@/modules/datasetDetails/DatasetDetailsIsUsedBy.vue";
+  import DatasetDetailsIsUsedBy from "@/modules/datasetDetails/DatasetDetailsIsUsedBy.vue";
 
   export default {
     name: 'datasetDetailsDataset',
     dependencies: 'DatasetService',
     components: {
+      DatasetDetailsIsUsedBy,
+      DatasetDetailsExtendedMetaData,
       DatasetDetailsMap,
       DatasetDetailsInfo,
       DatasetDetailsSubject,
@@ -592,9 +572,6 @@
           incrementSteps: this.$env.datasetDetails.dataServices.incrementSteps,
           descriptionMaxLines: this.$env.datasetDetails.dataServices.descriptionMaxLines,
           descriptionMaxChars: this.$env.datasetDetails.dataServices.descriptionMaxChars,
-        },
-        isUsedBy: {
-          isVisible: this.$env.datasetDetails.isUsedBy.isVisible,
         }
       };
     },
@@ -1165,9 +1142,6 @@
       },
       toggleDataServices() {
         this.dataServices.isVisible = !this.dataServices.isVisible;
-      },
-      toggleIsUsedBy() {
-        this.isUsedBy.isVisible = !this.isUsedBy.isVisible;
       },
       setTranslationBanners() {
         if (!this.$i18n) return;
---
+++ src/modules/datasetDetails/DatasetDetailsIsUsedBy.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="mt-5">
+    <div class="row">
+      <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
+           @click="toggleIsUsedBy()">
+          <span class="arrow text-dark"
+                v-if="!isUsedBy.isVisible">
+            <i class="material-icons">keyboard_arrow_down</i>
+          </span>
+        <span class="arrow text-dark" v-else>
+            <i class="material-icons">keyboard_arrow_up</i>
+          </span>
+      </div>
+      <div class="col-11 py-2 bg-white">
+        <h2 class="heading"
+            data-cy="documentation-toggle"
+            @click="toggleIsUsedBy()">{{ $t('message.datasetDetails.isUsedBy') }} ({{ getExtendedMetadata ? getExtendedMetadata.is_used_by.length.toLocaleString('fi') : 0 }})</h2>
+      </div>
+      <span v-if="isUsedBy.isVisible" class="list list-unstyled col-12">
+          <hr>
+            <ul class="sectionList" v-if="getExtendedMetadata.is_used_by.length != null || getExtendedMetadata.is_used_by != undefined ">
+                <li v-for="(link, i) in getExtendedMetadata.is_used_by" :key="i">
+                  <a :href="link" target="_blank">{{link}}</a>
+                </li>
+            </ul>
+          <hr>
+        </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DatasetDetailsIsUsedBy",
+  props: {
+    getExtendedMetadata: Object
+  },
+  data() {
+    return {
+      isUsedBy: {
+        isVisible: this.$env.datasetDetails.isUsedBy.isVisible,
+      }
+    };
+  },
+  methods: {
+    toggleIsUsedBy() {
+      this.isUsedBy.isVisible = !this.isUsedBy.isVisible;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.heading {
+  cursor: pointer;
+}
+</style>
