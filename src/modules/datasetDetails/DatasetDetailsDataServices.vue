2b99cefd1ffc3de15c64eba0064e9908cbaa084c
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-09-14 12:56:27 +0200

    refactor: created DatasetDetailsDataServices.vue

---
+++ src/modules/datasetDetails/DatasetDetailsDataServices.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="row mt-5">
+    <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
+         @click="toggleDataServices()">
+        <span class="arrow text-dark"
+              v-if="!dataServices.isVisible">
+          <i class="material-icons">keyboard_arrow_down</i>
+        </span>
+      <span class="arrow text-dark" v-else>
+          <i class="material-icons">keyboard_arrow_up</i>
+        </span>
+    </div>
+    <div class="col-11 py-2 bg-white">
+      <h2 class="heading"
+          data-cy="documentation-toggle"
+          @click="toggleDataServices()">{{ $t('message.metadata.dataServices') }}  ({{ getDataServices ? getDataServices.length.toLocaleString('fi') : 0 }})</h2>
+    </div>
+    <ul v-if="dataServices.isVisible" class="list list-unstyled col-12" data-cy="documentation">
+      <hr>
+      <div class="distributions" :key="`${expandedDataServices.length}--${expandedDataServicesDescriptions.length}`">
+        <div
+          v-for="(dataService, i) in displayedDataServices"
+          :key="i"
+          class="distributions__item"
+        >
+          <!-- Preview and action overlay -->
+          <div
+            v-if="!dataServices.displayAll && !isDataServicesAllDisplayed && i === dataServices.displayCount - 1"
+            class="distributions__item--preview"
+          >
+            <!-- Fade out the last item so it has a preview feel -->
+            <!-- Render actions on top of it -->
+            <div class="distributions__actions pb-md-3">
+              <button
+                v-for="increment in dataServices.incrementSteps.filter(nonOverflowingIncrementsForPages)"
+                :key="increment"
+                class="btn btn-sm btn-secondary mr-1"
+                @click="increaseNumDisplayedPages(increment)"
+              >
+                <i class="fas fa-chevron-down"/> {{ $t('message.metadata.showXMore', { increment }) }}
+              </button>
+              <button
+                class="btn btn-sm btn-primary"
+                @click="dataServices.displayCount = getDataServices.length"
+              >
+                <i class="fas fa-eye"/> {{ $t('message.metadata.showAll') }} {{ getDataServices.length.toLocaleString('fi') }}
+              </button>
+            </div>
+          </div>
+          <li class="row">
+            <!-- DATA SERVICE FORMAT PLACEHOLDER -->
+            <span class="d-inline-block col-2 col-md-1 pl-1 p-md-3 px-md-4 m-md-0 m-auto">
+                <div class="circle float-md-right text-center text-white invisible"/>
+              </span>
+            <span class="col-10 col-md-11">
+                <span class="row">
+                  <!-- DATA SERVICE TITLE -->
+                  <span class="d-inline-block col-12">
+                    <h3 class="m-0 text-break" :title="getDataServiceTitle(dataService)">{{ getDataServiceTitle(dataService) }}</h3>
+                  </span>
+                  <span class="d-inline-block col-12 col-md-9 col-lg-7">
+                    <!-- DATA SERVICE DESCRIPTION -->
+                    <span class="mt-2 d-block">
+                      <small v-if="dataServiceDescriptionIsExpanded(`data-service-description-toggle-${i}`)">
+                        <p class="text-muted">
+                          {{ getDataServiceDescription(dataService) }}
+                          <span class="text-primary details-link pl-2" @click="toggleDataServiceDescription(`data-service-description-toggle-${i}`)">
+                            {{ $t('message.metadata.readLess') }}
+                          </span>
+                        </p>
+                      </small>
+                      <small v-else-if="!dataServiceDescriptionIsExpandable(getDataServiceDescription(dataService))">
+                        <p class="text-muted">{{ getDataServiceDescription(dataService) }}</p>
+                      </small>
+                      <small v-else>
+                        <p class="text-muted">
+                          {{ truncate(getDataServiceDescription(dataService), dataService.descriptionMaxChars) }}
+                          <span class="text-primary details-link pl-2" @click="toggleDataServiceDescription(`data-service-description-toggle-${i}`)">
+                            {{ $t('message.metadata.readMore') }}
+                          </span>
+                        </p>
+                      </small>
+                    </span>
+                  </span>
+                  <!-- DATA SERVICE BUTTONS -->
+                  <span class="col-12 col-md-3 col-lg-5 mt-2 text-md-right text-left">
+                    <!-- DATA SERVICE ACCESS -->
+                    <span class="download dropdown d-inline-block">
+                      <app-link class="btn btn-sm btn-primary p-0 pl-2 pr-2 w-100 rounded-lg btn-color"
+                                :to="dataService.endpoint_url[0]"
+                                target="_blank"
+                                rel="dcat:distribution noopener"
+                                matomo-track-download
+                                @after-click="trackGoto">
+                            {{ $t('message.datasetDetails.access') }}
+                      </app-link>
+                    </span>
+                  </span>
+                </span>
+              </span>
+          </li>
+          <hr class="mt-1">
+        </div>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {getTranslationFor, truncate} from "@/modules/utils/helpers";
+import {has, isNil} from "lodash";
+import AppLink from "@/modules/widgets/AppLink";
+
+export default {
+  name: "DatasetDetailsDataServices",
+  components: {AppLink},
+  props: {
+    getDataServices: Array,
+    nonOverflowingIncrementsForPages: Function,
+    increaseNumDisplayedPages: Function,
+    trackGoto: Function
+  },
+  data() {
+    return {
+      dataServices: {
+        isVisible: this.$env.datasetDetails.dataServices.isVisible,
+        displayAll: this.$env.datasetDetails.dataServices.displayAll,
+        displayCount: this.$env.datasetDetails.dataServices.displayCount,
+        incrementSteps: this.$env.datasetDetails.dataServices.incrementSteps,
+        descriptionMaxLines: this.$env.datasetDetails.dataServices.descriptionMaxLines,
+        descriptionMaxChars: this.$env.datasetDetails.dataServices.descriptionMaxChars,
+      },
+      expandedDataServicesDescriptions: [],
+      expandedDataServices: [],
+    };
+  },
+  computed: {
+    displayedDataServices() {
+      const sorted = [...this.getDataServices].sort((a, b) => {
+        if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) < getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return -1; }
+        if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) > getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return 1; }
+        return 0;
+      });
+      return this.dataServices.displayAll
+        ? Object.freeze(sorted)
+        : Object.freeze(sorted.slice(0, this.dataServices.displayCount));
+    },
+    isDataServicesAllDisplayed() {
+      return this.dataServices.displayCount === this.getDataServices.length;
+    },
+    remainingDataServices() {
+      return this.getDataServices.length - this.dataServices.displayCount;
+    }
+  },
+  methods: {
+    truncate,
+    toggleDataServiceDescription(id) {
+      const index = this.expandedDataServicesDescriptions.indexOf(id);
+      if (index > -1) this.expandedDataServicesDescriptions.splice(index, 1);
+      else this.expandedDataServicesDescriptions.push(id);
+    },
+    toggleDataServices() {
+      this.dataServices.isVisible = !this.dataServices.isVisible;
+    },
+    dataServiceIsExpanded(id) {
+      return this.expandedDataServices.includes(id);
+    },
+    dataServiceDescriptionIsExpanded(id) {
+      return this.expandedDataServicesDescriptions.includes(id);
+    },
+    dataServiceDescriptionIsExpandable(description) {
+      return isNil(description) ? false : description.length > this.dataServices.descriptionMaxChars;
+    },
+    getDataServiceTitle(dataService) {
+      return dataService.title ? getTranslationFor(dataService.title, this.$route.query.locale, this.getLanguages) : truncate(dataService.endpoint_url[0], 50);
+    },
+    getDataServiceDescription(dataService) {
+      return (has(dataService, 'description') && !isNil(dataService.description)) ? getTranslationFor(dataService.description, this.$route.query.locale, this.getLanguages) : this.$t('message.catalogsAndDatasets.noDescriptionAvailable');
+    }
+  },
+  mounted() {
+    const dataServicesConf = this.$env.datasetDetails && this.$env.datasetDetails.dataServices;
+    if (dataServicesConf) {
+      this.dataServices.displayAll = dataServicesConf.displayAll || this.dataServices.displayAll;
+      this.dataServices.displayCount = parseInt(dataServicesConf.displayCount, 10) || this.dataServices.displayCount;
+
+      if (typeof dataServicesConf.incrementSteps === 'string') {
+        // Need to parse as json array since env variables are string only
+        dataServicesConf.incrementSteps = JSON.parse(dataServicesConf.incrementSteps);
+      }
+      this.dataServices.incrementSteps = dataServicesConf.incrementSteps || this.dataServices.incrementSteps;
+    }
+
+    if (this.dataServices.displayAll) this.dataServices.displayCount = this.getDataServices.length;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/modules/datasetDetails/DatasetDetailsDataset.vue
+++ src/modules/datasetDetails/DatasetDetailsDataset.vue
@@ -296,114 +296,13 @@
       </div>
     </div>
 
-    <!-- DATA SERVICES -->
-    <div class="mt-5" v-if="showObjectArray(getDataServices)">
-      <div class="row">
-        <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
-              @click="toggleDataServices()">
-          <span class="arrow text-dark"
-                v-if="!dataServices.isVisible">
-            <i class="material-icons">keyboard_arrow_down</i>
-          </span>
-          <span class="arrow text-dark" v-else>
-            <i class="material-icons">keyboard_arrow_up</i>
-          </span>
-        </div>
-        <div class="col-11 py-2 bg-white">
-          <h2 class="heading"
-              data-cy="documentation-toggle"
-              @click="toggleDataServices()">{{ $t('message.metadata.dataServices') }}  ({{ getDataServices ? getDataServices.length.toLocaleString('fi') : 0 }})</h2>
-        </div>
-        <ul v-if="dataServices.isVisible" class="list list-unstyled col-12" data-cy="documentation">
-          <hr>
-          <div class="distributions" :key="`${expandedDataServices.length}--${expandedDataServicesDescriptions.length}`">
-            <div
-              v-for="(dataService, i) in displayedDataServices"
-              :key="i"
-              class="distributions__item"
-            >
-              <!-- Preview and action overlay -->
-              <div
-                v-if="!dataServices.displayAll && !isDataServicesAllDisplayed && i === dataServices.displayCount - 1"
-                class="distributions__item--preview"
-              >
-                <!-- Fade out the last item so it has a preview feel -->
-                <!-- Render actions on top of it -->
-                <div class="distributions__actions pb-md-3">
-                  <button
-                    v-for="increment in dataServices.incrementSteps.filter(nonOverflowingIncrementsForPages)"
-                    :key="increment"
-                    class="btn btn-sm btn-secondary mr-1"
-                    @click="increaseNumDisplayedPages(increment)"
-                  >
-                    <i class="fas fa-chevron-down"/> {{ $t('message.metadata.showXMore', { increment }) }}
-                  </button>
-                  <button
-                    class="btn btn-sm btn-primary"
-                    @click="dataServices.displayCount = getDataServices.length"
-                  >
-                    <i class="fas fa-eye"/> {{ $t('message.metadata.showAll') }} {{ getDataServices.length.toLocaleString('fi') }}
-                  </button>
-                </div>
-              </div>
-              <li class="row">
-                <!-- DATA SERVICE FORMAT PLACEHOLDER -->
-                <span class="d-inline-block col-2 col-md-1 pl-1 p-md-3 px-md-4 m-md-0 m-auto">
-                  <div class="circle float-md-right text-center text-white invisible"/>
-                </span>
-                <span class="col-10 col-md-11">
-                  <span class="row">
-                    <!-- DATA SERVICE TITLE -->
-                    <span class="d-inline-block col-12">
-                      <h3 class="m-0 text-break" :title="getDataServiceTitle(dataService)">{{ getDataServiceTitle(dataService) }}</h3>
-                    </span>
-                    <span class="d-inline-block col-12 col-md-9 col-lg-7">
-                      <!-- DATA SERVICE DESCRIPTION -->
-                      <span class="mt-2 d-block">
-                        <small v-if="dataServiceDescriptionIsExpanded(`data-service-description-toggle-${i}`)">
-                          <p class="text-muted">
-                            {{ getDataServiceDescription(dataService) }}
-                            <span class="text-primary details-link pl-2" @click="toggleDataServiceDescription(`data-service-description-toggle-${i}`)">
-                              {{ $t('message.metadata.readLess') }}
-                            </span>
-                          </p>
-                        </small>
-                        <small v-else-if="!dataServiceDescriptionIsExpandable(getDataServiceDescription(dataService))">
-                          <p class="text-muted">{{ getDataServiceDescription(dataService) }}</p>
-                        </small>
-                        <small v-else>
-                          <p class="text-muted">
-                            {{ truncate(getDataServiceDescription(dataService), dataService.descriptionMaxChars) }}
-                            <span class="text-primary details-link pl-2" @click="toggleDataServiceDescription(`data-service-description-toggle-${i}`)">
-                              {{ $t('message.metadata.readMore') }}
-                            </span>
-                          </p>
-                        </small>
-                      </span>
-                    </span>
-                    <!-- DATA SERVICE BUTTONS -->
-                    <span class="col-12 col-md-3 col-lg-5 mt-2 text-md-right text-left">
-                      <!-- DATA SERVICE ACCESS -->
-                      <span class="download dropdown d-inline-block">
-                        <app-link class="btn btn-sm btn-primary p-0 pl-2 pr-2 w-100 rounded-lg btn-color"
-                                  :to="dataService.endpoint_url[0]"
-                                  target="_blank"
-                                  rel="dcat:distribution noopener"
-                                  matomo-track-download
-                                  @after-click="trackGoto">
-                              {{ $t('message.datasetDetails.access') }}
-                        </app-link>
-                      </span>
-                    </span>
-                  </span>
-                </span>
-              </li>
-              <hr class="mt-1">
-            </div>
-          </div>
-        </ul>
-      </div>
-    </div>
+    <dataset-details-data-services
+      v-if="showObjectArray(getDataServices)"
+      :getDataServices="getDataServices"
+      :nonOverflowingIncrementsForPages="nonOverflowingIncrementsForPages"
+      :increaseNumDisplayedPages="increaseNumDisplayedPages"
+      :trackGoto="trackGoto"
+    />
 
     <dataset-details-is-used-by
       v-if="showObject(getExtendedMetadata)"
@@ -463,11 +362,13 @@
   import DatasetDetailsExtendedMetaData
     from "@/modules/datasetDetails/DatasetDetailsIsUsedBy.vue";
   import DatasetDetailsIsUsedBy from "@/modules/datasetDetails/DatasetDetailsIsUsedBy.vue";
+  import DatasetDetailsDataServices from "@/modules/datasetDetails/DatasetDetailsDataServices.vue";
 
   export default {
     name: 'datasetDetailsDataset',
     dependencies: 'DatasetService',
     components: {
+      DatasetDetailsDataServices,
       DatasetDetailsIsUsedBy,
       DatasetDetailsExtendedMetaData,
       DatasetDetailsMap,
@@ -528,8 +429,6 @@
         expandedPageDescriptions: [],
         expandedVisualisations: [],
         expandedVisualisationDescriptions: [],
-        expandedDataServices: [],
-        expandedDataServicesDescriptions: [],
         visualisationLinkFormats: [
           'csv',
           'xlsx',
@@ -564,14 +463,6 @@
           incrementSteps: this.$env.datasetDetails.visualisations.incrementSteps,
           descriptionMaxLines: this.$env.datasetDetails.visualisations.descriptionMaxLines,
           descriptionMaxChars: this.$env.datasetDetails.visualisations.descriptionMaxChars,
-        },
-        dataServices: {
-          isVisible: this.$env.datasetDetails.dataServices.isVisible,
-          displayAll: this.$env.datasetDetails.dataServices.displayAll,
-          displayCount: this.$env.datasetDetails.dataServices.displayCount,
-          incrementSteps: this.$env.datasetDetails.dataServices.incrementSteps,
-          descriptionMaxLines: this.$env.datasetDetails.dataServices.descriptionMaxLines,
-          descriptionMaxChars: this.$env.datasetDetails.dataServices.descriptionMaxChars,
         }
       };
     },
@@ -670,22 +561,6 @@
       },
       remainingVisualisations() {
         return this.getVisualisations.length - this.visualisations.displayCount;
-      },
-      displayedDataServices() {
-        const sorted = [...this.getDataServices].sort((a, b) => {
-          if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) < getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return -1; }
-          if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) > getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return 1; }
-          return 0;
-        });
-        return this.dataServices.displayAll
-          ? Object.freeze(sorted)
-          : Object.freeze(sorted.slice(0, this.dataServices.displayCount));
-      },
-      isDataServicesAllDisplayed() {
-        return this.dataServices.displayCount === this.getDataServices.length;
-      },
-      remainingDataServices() {
-        return this.getDataServices.length - this.dataServices.displayCount;
       }
     },
     methods: {
@@ -1081,21 +956,6 @@
         const accessUrl = distribution.downloadUrls && distribution.downloadUrls.length ? distribution.downloadUrls[0] : distribution.accessUrl[0];
         return `/data/visualisation/?file=${encodeURIComponent(accessUrl)}`;
       },
-      dataServiceIsExpanded(id) {
-        return this.expandedDataServices.includes(id);
-      },
-      dataServiceDescriptionIsExpanded(id) {
-        return this.expandedDataServicesDescriptions.includes(id);
-      },
-      dataServiceDescriptionIsExpandable(description) {
-        return isNil(description) ? false : description.length > this.dataServices.descriptionMaxChars;
-      },
-      getDataServiceTitle(dataService) {
-        return dataService.title ? getTranslationFor(dataService.title, this.$route.query.locale, this.getLanguages) : truncate(dataService.endpoint_url[0], 50);
-      },
-      getDataServiceDescription(dataService) {
-        return (has(dataService, 'description') && !isNil(dataService.description)) ? getTranslationFor(dataService.description, this.$route.query.locale, this.getLanguages) : this.$t('message.catalogsAndDatasets.noDescriptionAvailable');
-      },
       getGeoLink(format, distributionID) {
         let f = format.toLowerCase();
         // Use correct Case Sensitive strings
@@ -1134,14 +994,6 @@
         const index = this.expandedVisualisationDescriptions.indexOf(id);
         if (index > -1) this.expandedVisualisationDescriptions.splice(index, 1);
         else this.expandedVisualisationDescriptions.push(id);
-      },
-      toggleDataServiceDescription(id) {
-        const index = this.expandedDataServicesDescriptions.indexOf(id);
-        if (index > -1) this.expandedDataServicesDescriptions.splice(index, 1);
-        else this.expandedDataServicesDescriptions.push(id);
-      },
-      toggleDataServices() {
-        this.dataServices.isVisible = !this.dataServices.isVisible;
       },
       setTranslationBanners() {
         if (!this.$i18n) return;
@@ -1340,20 +1192,6 @@
       }
 
       if (this.pages.displayAll) this.pages.displayCount = this.getPages.length;
-
-      const dataServicesConf = this.$env.datasetDetails && this.$env.datasetDetails.dataServices;
-      if (dataServicesConf) {
-        this.dataServices.displayAll = dataServicesConf.displayAll || this.dataServices.displayAll;
-        this.dataServices.displayCount = parseInt(dataServicesConf.displayCount, 10) || this.dataServices.displayCount;
-
-        if (typeof dataServicesConf.incrementSteps === 'string') {
-          // Need to parse as json array since env variables are string only
-          dataServicesConf.incrementSteps = JSON.parse(dataServicesConf.incrementSteps);
-        }
-        this.dataServices.incrementSteps = dataServicesConf.incrementSteps || this.dataServices.incrementSteps;
-      }
-
-      if (this.dataServices.displayAll) this.dataServices.displayCount = this.getDataServices.length;
     },
     beforeDestroy() {
       $('.tooltip').remove();
