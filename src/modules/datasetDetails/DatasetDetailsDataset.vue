685f1adf7dc66c2d43f63549406b42795a360cd0
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-09-14 12:24:06 +0200

    refactor: created DatasetDetailsRelations.vue

--- src/modules/datasetDetails/DatasetDetailsDataset.vue
+++ src/modules/datasetDetails/DatasetDetailsDataset.vue
@@ -435,34 +435,10 @@
     </div>
 
     <!-- DATA RELATED RESOURCE -->
-    <div class="mt-5" v-if="showArray(getRelations)">
-      <div class="row">
-        <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
-              @click="toggleRelatedResources()">
-          <span class="arrow text-dark"
-                v-if="!relatedResources.isVisible">
-            <i class="material-icons">keyboard_arrow_down</i>
-          </span>
-          <span class="arrow text-dark" v-else>
-            <i class="material-icons">keyboard_arrow_up</i>
-          </span>
-        </div>
-        <div class="col-11 py-2 bg-white">
-          <h2 class="heading"
-              data-cy="documentation-toggle"
-              @click="toggleRelatedResources()">{{ $t('message.datasetDetails.relatedResources') }} ({{ getRelations ? getRelations.length.toLocaleString('fi') : 0 }})</h2>
-        </div>
-        <span v-if="relatedResources.isVisible" class="list list-unstyled col-12">
-          <hr>
-            <ul class="sectionList" v-if="getRelations.length != null || getRelations != undefined ">
-                <li v-for="(link, i) in getRelations" :key="i">
-                  <a :href="link" target="_blank">{{link}}</a>
-                </li>
-            </ul>
-          <hr>
-        </span>
-      </div>
-    </div>
+    <dataset-details-relations
+      v-if="showArray(getRelations)"
+      :getRelations="getRelations"
+    />
 
     <dataset-details-map
       v-if="showObjectArray(getSpatial)"
@@ -619,9 +595,6 @@
         },
         isUsedBy: {
           isVisible: this.$env.datasetDetails.isUsedBy.isVisible,
-        },
-        relatedResources: {
-          isVisible: this.$env.datasetDetails.relatedResources.isVisible,
         }
       };
     },
@@ -1195,9 +1168,6 @@
       },
       toggleIsUsedBy() {
         this.isUsedBy.isVisible = !this.isUsedBy.isVisible;
-      },
-      toggleRelatedResources() {
-        this.relatedResources.isVisible = !this.relatedResources.isVisible;
       },
       setTranslationBanners() {
         if (!this.$i18n) return;
---
+++ src/modules/datasetDetails/DatasetDetailsRelations.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row mt-5">
+    <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
+         @click="toggleRelatedResources()">
+        <span class="arrow text-dark"
+              v-if="!relatedResources.isVisible">
+          <i class="material-icons">keyboard_arrow_down</i>
+        </span>
+      <span class="arrow text-dark" v-else>
+          <i class="material-icons">keyboard_arrow_up</i>
+        </span>
+    </div>
+    <div class="col-11 py-2 bg-white">
+      <h2 class="heading"
+          data-cy="documentation-toggle"
+          @click="toggleRelatedResources()">{{ $t('message.datasetDetails.relatedResources') }} ({{ getRelations ? getRelations.length.toLocaleString('fi') : 0 }})</h2>
+    </div>
+    <span v-if="relatedResources.isVisible" class="list list-unstyled col-12">
+        <hr>
+          <ul class="sectionList" v-if="getRelations.length != null || getRelations != undefined ">
+              <li v-for="(link, i) in getRelations" :key="i">
+                <a :href="link" target="_blank">{{link}}</a>
+              </li>
+          </ul>
+        <hr>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DatasetDetailsRelations",
+  props: {
+    getRelations: Array
+  },
+  data() {
+    return {
+      relatedResources: {
+        isVisible: this.$env.datasetDetails.relatedResources.isVisible,
+      }
+    }
+  },
+  methods: {
+    toggleRelatedResources() {
+      this.relatedResources.isVisible = !this.relatedResources.isVisible;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.heading {
+  cursor: pointer;
+}
+</style>
