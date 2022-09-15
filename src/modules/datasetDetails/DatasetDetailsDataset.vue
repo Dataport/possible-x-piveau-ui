d35ed65575a6440fdf3fbdb93233b151c1d8f55d
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-09-15 11:24:30 +0200

    refactor: created DatasetDetailsPages.vue

--- src/modules/datasetDetails/DatasetDetailsDataset.vue
+++ src/modules/datasetDetails/DatasetDetailsDataset.vue
@@ -67,120 +67,14 @@
       :sortAlphabetically="sortAlphabetically"
     />
 
-    <!-- PAGES -->
-    <div class="mt-5" v-if="showObjectArray(getPages)">
-      <div class="row">
-        <div class="d-none d-lg-block col-1 my-auto pr-0 text-right"
-              @click="togglePage()">
-          <span class="arrow text-dark"
-                v-if="!pages.isVisible">
-            <i class="material-icons">keyboard_arrow_down</i>
-          </span>
-          <span class="arrow text-dark" v-else>
-            <i class="material-icons">keyboard_arrow_up</i>
-          </span>
-        </div>
-        <div class="col-11 py-2 bg-white">
-          <h2 class="heading"
-              data-cy="documentation-toggle"
-              @click="togglePage()">{{ $t('message.metadata.documentations') }}  ({{ getPages ? getPages.length.toLocaleString('fi') : 0 }})</h2>
-        </div>
-        <ul v-if="pages.isVisible" class="list list-unstyled col-12" data-cy="documentation">
-          <hr>
-          <div class="distributions" :key="`${expandedPages.length}--${expandedPageDescriptions.length}`">
-            <div
-              v-for="(page, i) in displayedPages"
-              :key="i"
-              class="distributions__item"
-            >
-              <!-- Preview and action overlay -->
-              <div
-                v-if="!pages.displayAll && !isPagesAllDisplayed && i === pages.displayCount - 1"
-                class="distributions__item--preview"
-              >
-                <!-- Fade out the last item so it has a preview feel -->
-                <!-- Render actions on top of it -->
-                <div class="distributions__actions pb-md-3">
-                  <button
-                    v-for="increment in pages.incrementSteps.filter(nonOverflowingIncrementsForPages)"
-                    :key="increment"
-                    class="btn btn-sm btn-secondary mr-1"
-                    @click="increaseNumDisplayedPages(increment)"
-                  >
-                    <i class="fas fa-chevron-down"/> {{ $t('message.metadata.showXMore', { increment }) }}
-                  </button>
-                  <button
-                    class="btn btn-sm btn-primary"
-                    @click="pages.displayCount = getPages.length"
-                  >
-                    <i class="fas fa-eye"/> {{ $t('message.metadata.showAll') }} {{ getPages.length.toLocaleString('fi') }}
-                  </button>
-                </div>
-              </div>
-              <li class="row">
-                <!-- PAGE FORMAT -->
-                <span class="d-inline-block col-2 col-md-1 pl-1 p-md-3 px-md-4 m-md-0 m-auto">
-                  <div class="circle float-md-right text-center text-white"
-                        :type="getPageFormat(page)"
-                        :data-toggle="pageFormatTruncated(page) ? 'tooltip' : false"
-                        :data-placement="pageFormatTruncated(page) ? 'top' : false"
-                        :title="pageFormatTruncated(page) ? getPageFormat(page) : false">
-                    <span>{{ truncate(getPageFormat(page), 4, true) }}</span>
-                  </div>
-                </span>
-                <span class="col-10 col-md-11">
-                  <span class="row">
-                    <!-- PAGE TITLE -->
-                    <span class="d-inline-block col-12">
-                      <h3 class="m-0 text-break" :title="getPageTitle(page)">{{ getPageTitle(page) }}</h3>
-                    </span>
-                    <span class="d-inline-block col-12 col-md-9 col-lg-7">
-                      <!-- PAGE DESCRIPTION -->
-                      <span class="mt-2 d-block">
-                        <small v-if="pageDescriptionIsExpanded(`page-description-toggle-${i}`)">
-                          <p class="text-muted">
-                            {{ getPageDescription(page) }}
-                            <span class="text-primary details-link pl-2" @click="togglePageDescription(`page-description-toggle-${i}`)">
-                              {{ $t('message.metadata.readLess') }}
-                            </span>
-                          </p>
-                        </small>
-                        <small v-else-if="!pageDescriptionIsExpandable(getPageDescription(page))">
-                          <p class="text-muted">{{ getPageDescription(page) }}</p>
-                        </small>
-                        <small v-else>
-                          <p class="text-muted">
-                            {{ truncate(getPageDescription(page), pages.descriptionMaxChars) }}
-                            <span class="text-primary details-link pl-2" @click="togglePageDescription(`page-description-toggle-${i}`)">
-                              {{ $t('message.metadata.readMore') }}
-                            </span>
-                          </p>
-                        </small>
-                      </span>
-                    </span>
-                    <!-- PAGE BUTTONS -->
-                    <span class="col-12 col-md-3 col-lg-5 mt-2 text-md-right text-left">
-                      <!-- PAGE ACCESS -->
-                      <span class="download dropdown d-inline-block">
-                        <app-link class="btn btn-sm btn-primary p-0 pl-2 pr-2 w-100 rounded-lg btn-color"
-                                  :to="page.resource"
-                                  target="_blank"
-                                  rel="dcat:distribution noopener"
-                                  matomo-track-download
-                                  @after-click="trackGoto">
-                              {{ $t('message.datasetDetails.access') }}
-                        </app-link>
-                      </span>
-                    </span>
-                  </span>
-                </span>
-              </li>
-              <hr class="mt-1">
-            </div>
-          </div>
-        </ul>
-      </div>
-    </div>
+    <dataset-details-pages
+      v-if="showObjectArray(getPages)"
+      :getPages="getPages"
+      :pages="pages"
+      :increaseNumDisplayedPages="increaseNumDisplayedPages"
+      :nonOverflowingIncrementsForPages="nonOverflowingIncrementsForPages"
+      :trackGoto="trackGoto"
+    />
 
     <dataset-details-visualisations
       v-if="showObjectArray(getVisualisations)"
@@ -257,11 +151,13 @@
   import DatasetDetailsDataServices from "@/modules/datasetDetails/DatasetDetailsDataServices.vue";
   import DatasetDetailsVisualisations
     from "@/modules/datasetDetails/DatasetDetailsVisualisations.vue";
+  import DatasetDetailsPages from "@/modules/datasetDetails/DatasetDetailsPages.vue";
 
   export default {
     name: 'datasetDetailsDataset',
     dependencies: 'DatasetService',
     components: {
+      DatasetDetailsPages,
       DatasetDetailsVisualisations,
       DatasetDetailsDataServices,
       DatasetDetailsIsUsedBy,
@@ -320,8 +216,6 @@
         translationNotAvailable: false,
         expandedDistributions: [],
         expandedDistributionDescriptions: [],
-        expandedPages: [],
-        expandedPageDescriptions: [],
         visualisationLinkFormats: [
           'csv',
           'xlsx',
@@ -414,22 +308,6 @@
       },
       remainingDistributions() {
         return this.getDistributions.length - this.distributions.displayCount;
-      },
-      displayedPages() {
-        const sorted = [...this.getPages].sort((a, b) => {
-          if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) < getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return -1; }
-          if (getTranslationFor(a.title, this.$route.query.locale, this.getLanguages) > getTranslationFor(b.title, this.$route.query.locale, this.getLanguages)) { return 1; }
-          return 0;
-        });
-        return this.pages.displayAll
-          ? Object.freeze(sorted)
-          : Object.freeze(sorted.slice(0, this.pages.displayCount));
-      },
-      isPagesAllDisplayed() {
-        return this.pages.displayCount === this.getPages.length;
-      },
-      remainingPages() {
-        return this.getPages.length - this.pages.displayCount;
       }
     },
     methods: {
@@ -778,27 +656,6 @@
       getDistributionDescription(distribution) {
         return (has(distribution, 'description') && !isNil(distribution.description)) ? getTranslationFor(distribution.description, this.$route.query.locale, this.getLanguages) : '-';
       },
-      pageIsExpanded(id) {
-        return this.expandedPages.includes(id);
-      },
-      pageDescriptionIsExpanded(id) {
-        return this.expandedPageDescriptions.includes(id);
-      },
-      pageDescriptionIsExpandable(description) {
-        return isNil(description) ? false : description.length > this.pages.descriptionMaxChars;
-      },
-      getPageFormat(page) {
-        return has(page, 'format.label') && !isNil(page.format.label) ? page.format.label : 'UNKNOWN';
-      },
-      pageFormatTruncated(page) {
-        return this.getPageFormat(page).length > 4;
-      },
-      getPageTitle(page) {
-        return page.title ? getTranslationFor(page.title, this.$route.query.locale, this.getLanguages) : truncate(page.resource, 50);
-      },
-      getPageDescription(page) {
-        return (has(page, 'description') && !isNil(page.description)) ? getTranslationFor(page.description, this.$route.query.locale, this.getLanguages) : this.$t('message.catalogsAndDatasets.noDescriptionAvailable');
-      },
       getVisualisationLink(distribution) {
         // Return Visualisation Link
         const accessUrl = distribution.downloadUrls && distribution.downloadUrls.length ? distribution.downloadUrls[0] : distribution.accessUrl[0];
@@ -823,17 +680,6 @@
         const index = this.expandedDistributionDescriptions.indexOf(id);
         if (index > -1) this.expandedDistributionDescriptions.splice(index, 1);
         else this.expandedDistributionDescriptions.push(id);
-      },
-      togglePageDescription(id) {
-        const index = this.expandedPageDescriptions.indexOf(id);
-        if (index > -1) this.expandedPageDescriptions.splice(index, 1);
-        else this.expandedPageDescriptions.push(id);
-      },
-      togglePage() {
-        this.pages.isVisible = !this.pages.isVisible;
-        $('[data-toggle="tooltip"]').tooltip({
-          container: 'body',
-        });
       },
       setTranslationBanners() {
         if (!this.$i18n) return;
