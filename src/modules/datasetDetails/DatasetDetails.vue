11aa82c749ea4f886cab126b2845ad4909e333ca
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-09-19 09:24:14 +0200

    refactor: created DatasetDetailsNavigationPage.vue

--- src/modules/datasetDetails/DatasetDetails.vue
+++ src/modules/datasetDetails/DatasetDetails.vue
@@ -54,8 +54,6 @@ export default {
     ]),
   },
   methods: {
-    ...mapActions('datasetDetails', [
-    ]),
     getRepresentativeLocaleOf,
     getTranslationFor,
   }
--- src/modules/datasetDetails/navigation/DatasetDetailsNavigationPages.vue
+++ src/modules/datasetDetails/navigation/DatasetDetailsNavigationPages.vue
@@ -1,69 +1,16 @@
 <template>
   <nav>
     <ul class="menu m-0 list-inline list-unstyled navbar-nav mr-auto">
-      <li
+      <li v-for="(page, index) in pages" :key="index"
         class="mr-2 list-inline-item nav-item text-nowrap"
       >
-        <app-link
-          class="nav-link"
-          :to="{ path: `/datasets/${getID}`, query: Object.assign({}, { locale: $route.query.locale }) }"
-          :title="$t('message.tooltip.datasetDetails.dataset')"
-          :exact="true"
-        >
-              <span :title="$t('message.tooltip.datasetDetails.dataset')"
-                    data-toggle="tooltip"
-                    data-placement="top">
-                      {{ $t('message.datasetDetails.subnav.dataset') }}
-              </span>
-        </app-link>
+        <dataset-details-navigation-page
+          :path="page.path"
+          :title="page.title"
+          :tooltip="page.tooltip"
+          :exact="page.exact"
+        />
       </li>
-      <li
-        class="mr-2 list-inline-item nav-item text-nowrap"
-      >
-        <app-link
-          class="nav-link"
-          :to="{ path: `/datasets/${getID}/categories`, query: Object.assign({}, { locale: $route.query.locale }) }"
-        >
-            <span :title="$t('message.tooltip.datasetDetails.categories')"
-                  data-toggle="tooltip"
-                  data-placement="top">
-                  {{ $t('message.datasetDetails.subnav.categories') }}
-            </span>
-        </app-link>
-      </li>
-      <li
-        class="mr-2 list-inline-item nav-item text-nowrap"
-      >
-        <app-link
-          class="nav-link"
-          :to="{ path: `/datasets/${getID}/similarDatasets`, query: Object.assign({}, { locale: $route.query.locale }) }"
-        >
-              <span :title="$t('message.tooltip.datasetDetails.similarDatasets')"
-                    data-toggle="tooltip"
-                    data-placement="top">
-                    {{ $t('message.datasetDetails.subnav.similarDatasets') }}
-              </span>
-        </app-link>
-      </li>
-      <li
-        class="mr-2 list-inline-item nav-item text-nowrap"
-      >
-        <app-link
-          class="nav-link"
-          :to="{ path: `/datasets/${getID}/quality`, query: Object.assign({}, { locale: $route.query.locale }) }"
-        >
-              <span :title="$t('message.tooltip.datasetDetails.quality')"
-                    data-toggle="tooltip"
-                    data-placement="top">
-                    {{ $t('message.datasetDetails.subnav.quality') }}
-              </span>
-        </app-link>
-      </li>
-      <!--<li class="mr-2 list-inline-item nav-item text-nowrap"-->
-      <!--<app-link class="nav-link" :to="{ path: `/datasets/${getID}/activityStream`, query: Object.assign({}, $route.query) }">-->
-      <!--{{ $t('message.datasetDetails.subnav.activityStream') }}-->
-      <!--</app-link>-->
-      <!--</li>-->
     </ul>
   </nav>
 </template>
@@ -71,13 +18,36 @@
 <script>
 import AppLink from "@/modules/widgets/AppLink";
 import {mapGetters} from "vuex";
+import Vue from "vue";
+import DatasetDetailsNavigationPage
+  from "@/modules/datasetDetails/navigation/DatasetDetailsNavigationPage";
 export default {
   name: "DatasetDetailsNavigationPages",
-  components: {AppLink},
+  components: {DatasetDetailsNavigationPage, AppLink},
+  // data() {
+  //   return {
+  //     pages: []
+  //   };
+  // },
   computed: {
     ...mapGetters('datasetDetails', [
       'getID'
-    ])
+    ]),
+    pages() {
+      const id = this.getID;
+      const createPage = (key, noPathKey, exact) => ({
+        path: `/datasets/${id}${noPathKey ? "" : `/${key}`}`,
+        title: Vue.i18n.t(`message.datasetDetails.subnav.${key}`),
+        tooltip: Vue.i18n.t(`message.tooltip.datasetDetails.${key}`),
+        exact
+      });
+      return [
+        createPage("dataset", true, true),
+        createPage("categories"),
+        createPage("similarDatasets"),
+        createPage("quality")
+      ];
+    }
   }
 }
 </script>
