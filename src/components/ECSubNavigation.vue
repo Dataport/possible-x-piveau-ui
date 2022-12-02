d2e628d7da8fed490a8334a28028deed2f6e3c90
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2022-12-02 14:41:30 +0100

    fix: changes in mobile view

---
+++ src/components/ECSubNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="col-12 navbar navbar-expand-lg navbar-light px-4 pb-3 mt-0 sub-navigation">
+    <button class="navbar-toggler px-1" type="button" data-toggle="collapse" data-target="#subNavigation" aria-controls="subNavigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <h1 class="d-inline-block d-lg-none col-10 col-md-11 title mb-0 text-break">{{ title }}</h1>
+    <div class="col-12 collapse navbar-collapse px-0" id="subNavigation">
+      <slot/>
+    </div>
+  </nav>
+</template>
+
+<script>
+import $ from "jquery";
+
+export default {
+  name: "ECSubNavigation",
+  props: {
+    to: {
+      default: '',
+      required: false,
+    },
+    title: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+  computed: {
+  },
+  methods: {
+  },
+  beforeDestroy() {
+    $('.tooltip').remove();
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
