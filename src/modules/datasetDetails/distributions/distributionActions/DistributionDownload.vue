af56b18e9882384560e37fcb507733d1dab1c6c4
Maksim Donic <admin@vpn3d109.fokus.fraunhofer.de>
2022-12-28 12:21:02 +0100

    fix: downloadAs feature

--- src/modules/datasetDetails/distributions/distributionActions/DistributionDownload.vue
+++ src/modules/datasetDetails/distributions/distributionActions/DistributionDownload.vue
@@ -44,7 +44,7 @@
         </app-link>
         <!-- <i class="material-icons help-icon ml-3" data-toggle="tooltip" data-placement="bottom" :title="$t('message.datasetDetails.downloadURLTooltip')">help_outline</i> -->
     </span>
-   <distribution-download-as :distribution="distribution" />
+   <distribution-download-as v-if="active" :distribution="distribution" />
   </distribution-dropdown-download>
 </template>
 
@@ -68,6 +68,11 @@ export default {
     DistributionDropdownDownload,
     DistributionDownloadAs,
     AppLink
+  },
+  data() {
+    return {
+      active: true,
+    }
   },
   methods: {
     setClipboard(value) {
