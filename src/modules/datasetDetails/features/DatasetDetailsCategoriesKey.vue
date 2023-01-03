b2ce59c8e4e493ae45e2f079a1e99ee46d98cd84
Michael Peters <michael.peters@fokus.fraunhofer.de>
2023-01-03 15:52:36 +0100

    feature: added new dsd feature

--- src/modules/datasetDetails/features/DatasetDetailsCategoriesKey.vue
+++ src/modules/datasetDetails/features/DatasetDetailsCategoriesKey.vue
@@ -1,141 +1,112 @@
 <template>
-  <div class="row mt-2 flex-column dsd-item dsd-keywords">
-        <span class="mb-4 h4 font-weight-bold">
-          {{ $t("message.datasetDetails.subnav.categories") }} ({{
-            getCategories.length
-          }})
+  <div class="row mt-2 flex-column dsd-feature">
+    <div>
+    <dataset-details-feature-header
+    :title="`${$t('message.datasetDetails.subnav.categories')} (${
+          getCategories ? getCategories.length.toLocaleString('fi') : 0
+        })`"
+    :arrowDown="isCategoriesAllDisplayed"
+    tag="keywords-toggle"
+    :onClick="toggleDisplayCount"
+  />
+    </div>
+    <div v-if="!isCategoriesAllDisplayed"  class="keywords__item row mt-4">
+        <span
+          v-for='(category, i) in getCategories'
+          :key="i"
+          class="col-6 col-sm-3 col-md-2 mt-md-0 mt-3 mb-2 px-1"
+        >
+        <app-link :to="getCategoryLink(category)" :rel="followCategoryLinks">
+            <small class="d-inline-block text-nowrap w-100 py-2 rounded-pill text-center text-white tag-color"
+                   :data-toggle="categoryTruncated(category) ? 'tooltip' : false"
+                   :data-placement="categoryTruncated(category) ? 'top' : false"
+                   :title="categoryTruncated(category) ? getTranslationFor(category.title, $route.query.locale) : false">
+              {{ truncate(getTranslationFor(category.title, $route.query.locale), maxCategoryLength, false) }}
+            </small>
+          </app-link>
         </span>
-        <div class="categories__item row" v-if="catCollapsed">
-          <span
-            v-for="(category, i) in getCategories"
-            :key="i"
-            class="col-6 col-sm-3 col-md-2 mt-md-0 mt-3 mb-2 px-1"
-          >
-            <app-link
-              :to="getCategoryLink(category)"
-              v-if="showCategory(category)"
-              ><!-- <small
-                class=" d-inline-block text-nowrap w-100 py-2 rounded-pill text-center text-white tag-color">
-                {{ getTranslationFor(category.title, $route.query.locale) }}
-              </small> -->
-              <small class="d-inline-block text-nowrap w-100 py-2 rounded-pill text-center text-white tag-color"
-                     :data-toggle="categoryTruncated(category) ? 'tooltip' : false"
-                     :data-placement="categoryTruncated(category) ? 'top' : false"
-                     :title="categoryTruncated(category) ? category.title : false">
-                {{ truncate(category.title, maxKeywordLength, false) }}
-              </small>
-            </app-link>
-          </span>
+    </div>
+    <div>
     </div>
   </div>
 </template>
 
 <script>
+import DatasetDetailsFeatureHeader from "@/modules/datasetDetails/features/DatasetDetailsFeatureHeader";
 import {truncate} from "@/modules/utils/helpers";
-import { mapActions, mapGetters } from "vuex";
-import { has } from "lodash-es";
-import { getTranslationFor } from "@/modules/utils/helpers";
-
+import {mapGetters} from "vuex";
 import AppLink from "@/modules/widgets/AppLink";
+import { getTranslationFor } from "@/modules/utils/helpers";
+import {sortAlphabetically} from "@/modules/datasetDetails/features/utils/sortAlphabetically";
+import $ from "jquery";
+
 
 export default {
-  name: "datasetDetailsCategoriesKey",
-  dependencies: "DatasetService",
-  components: {AppLink},
-  metaInfo() {
-    return {
-      title: this.$t("message.metadata.categories"),
-      showTitle: this.$env.datasetDetails.categories.showTitle,
-      meta: [
-        {
-          name: "description",
-          vmid: "description",
-          content: `${this.$t(
-            "message.metadata.categories"
-          )} - ${this.getTranslationFor(
-            this.getTitle,
-            this.$route.query.locale,
-            this.getLanguages
-          )} - data.europa.eu`.substr(0, 4999),
-        },
-        {
-          name: "keywords",
-          vmid: "keywords",
-          content: this.getKeywords
-            .map((k) => k.title)
-            .join(" ")
-            .substring(0, 4999),
-        },
-      ],
-    };
+name: "DatasetDetailsCategoriesKey",
+components: {AppLink,DatasetDetailsFeatureHeader},
+props: {
+  showCategory: Function
+},
+data() {
+  return {
+    defaultLocale: this.$env.languages.locale,
+    defaultDisplayCount: 0,
+    categories: {
+      displayAll: false,
+      displayCount: 24, // Should never exceed number of keywords
+      incrementSteps: [12, 60],
+    },
+    maxCategoryLength: 15,
+    followCategoryLinks: this.$env.datasets.followCategoryLinks
+  }
+},
+computed: {
+  ...mapGetters('datasetDetails', [
+      "getCategories"
+  ]),
+  isCategoriesAllDisplayed() {
+    return this.categories.displayCount >= this.getCategories.length;
+  } 
+},
+methods: {
+  truncate,
+  sortAlphabetically,
+  // Increases the current number of keywords displayed
+  // and clamps the result so that it never exceeds the number of all keywords.
+  increaseNumDisplayedKeywords(increment) {
+    const clampedSum = this.clamp(this.categories.displayCount + increment, 0, this.getCategories.length);
+    this.categories.displayCount = clampedSum;
   },
-  data() {
-    return {
-      catCollapsed:Boolean,
-      maxKeywordLength: this.$env.datasets.maxKeywordLength
-    };
+  getTranslationFor,
+  nonOverflowingIncrementsForKeywords(incrementStep) {
+    return this.categories.displayCount + incrementStep <= this.getCategories.length;
+  },
+  categoryTruncated(category) {
+    
+    return getTranslationFor(category.title, this.defaultLocale).length > this.maxCategoryLength;
     
   },
-  computed: {
-    // import store-getters
-    ...mapGetters("datasetDetails", [
-      "getCategories",
-      "getKeywords",
-      "getLanguages",
-      "getTitle",
-    ]),
-    isCategoriesAllDisplayed() {
-      return this.categories.displayCount >= this.getKeywords.length;
-    }
+  clamp(n, min, max) {
+    return Math.min(Math.max(n, min), max);
   },
-  methods: {
-    truncate,
-    has,
-    getTranslationFor,
-    // import store-actions
-    ...mapActions("datasetDetails", ["loadDatasetDetails", "useService"]),
-    showCategory(category) {
-      return has(category, "id");
-    },
-    getCategoryLink(category) {
+  getCategoryLink(category) {
       const categoryID = category.id.toUpperCase();
       return {
         path: `/datasets?categories=${categoryID}`,
         query: Object.assign({}, { locale: this.$route.query.locale }),
       };
     },
-    categoryTruncated(category) {
-      return category.title.length > this.maxKeywordLength;
-    }
-  },
-  created() {
-    this.useService(this.DatasetService);
-    this.$nextTick(() => {
-      this.$Progress.start();
-      this.loadDatasetDetails(this.$route.params.ds_id)
-        .then(() => {
-          this.$Progress.finish();
-        })
-        .catch(() => {
-          this.$Progress.fail();
-          this.$router.replace({
-            name: "NotFound",
-            query: {
-              locale: this.$route.query.locale,
-              dataset: this.$route.params.ds_id,
-            },
-          });
-        });
-    });
-  },
   toggleDisplayCount() {
-      if (this.categories.displayCount < this.getCategoryLink.length) {
-        this.categories.displayCount = this.getCategoryLink.length;
-      } else {
-        this.categories.displayCount = this.defaultDisplayCount;
-      }
+    $('body').tooltip({selector: '[data-toggle="tooltip"]'});
+    if (this.categories.displayCount < this.getCategories.length) {
+      this.categories.displayCount = this.getCategories.length;
+    } else {
+      this.categories.displayCount = this.defaultDisplayCount;
     }
-};
+    
+  }
+}
+}
 </script>
 
 <style scoped lang="scss">
