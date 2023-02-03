0f775a9e2bd2a6b35d45580663bc04efd29dd359
Bianca Wentzel <bianca.wentzel@fokus.fraunhofer.de>
2023-02-03 13:49:32 +0100

    refactor: cleaned up overview page

---
+++ src/modules/data-provider-interface/views/OverviewPage/Properties/URIProp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- SINGULAR URIs -->
+    <td v-if="value.type === 'singularURI'">{{ data[property]}}</td>
+
+    <!-- MULTIPLE URIs -->
+    <td v-if="value.type === 'multiURI'">
+        <div v-for="(el, index) in data[property]" :key="index">
+            {{ el }}
+        </div>
+    </td>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        property: String,
+        value: Object,
+        data: Object,
+    },
+}
+</script>
