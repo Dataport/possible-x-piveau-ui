f81900eedcccf92d1d2512a6ea7fca1a3eca74a9
Michael Peters <michael.peters@fokus.fraunhofer.de>
2023-04-24 10:36:45 +0200

    safety commit

--- src/modules/data-provider-interface/components/UniqueIdentifierInput.vue
+++ src/modules/data-provider-interface/components/UniqueIdentifierInput.vue
@@ -82,11 +82,15 @@ export default {
           const request = `${this.$env.api.hubUrl}${this.$route.params.property}/${this.uniqueID}?useNormalizedId=true`;
           axios.head(request)
             .then(() => {
+              
               this.isUniqueID = false;
             })
-            .catch(() => {
+            .catch((e) => {
+              if (e.response) {
+                console.log(e);
+              }
               this.isUniqueID = true;
-            });
+            } );
         }
       });
     },
