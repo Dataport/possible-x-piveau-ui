3a64bbf22ad23b1a4f0b61b70f055078fac6c6ad
msg <majid.salehi.ghamsari@fokus.fraunhofer.de>
2023-05-04 15:51:35 +0200

    fix: msg issue 2849 added more translations for 4040 page

--- src/modules/pages/NotFound.vue
+++ src/modules/pages/NotFound.vue
@@ -6,7 +6,7 @@
       <div class="col-10 offset-1">
         <slot>
           <h1>{{ $t('message.notFound.error') }} 404</h1>
-          <p>{{ $t('message.notFound.requested.partone')}} <app-link :to="{name: 'Datasets'}"> {{ $t('message.notFound.requested.parttwo')}} </app-link></p>
+          <p><app-link :to="{name: 'Datasets'}"> {{ $t('message.notFound.requested.page')}} </app-link></p>
           <p><app-link :href="`/${$route.query.locale || 'en'}/feedback/form`">{{ $t('message.notFound.contactUsAbout')}}</app-link></p>
         </slot>
       </div>
