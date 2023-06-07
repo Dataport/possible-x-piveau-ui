44567bfaa87ef7b70a0f038e5940c2f1952883e7
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2023-06-07 08:11:24 +0200

    slight improvement of create_links.js script

--- packages/piveau-hub-ui-modules/scripts/create_links.js
+++ packages/piveau-hub-ui-modules/scripts/create_links.js
@@ -1,19 +1,30 @@
+// Creates a dist folder with links to the contents of the lib folder
+// to allow a development workflow with consuming apps in the monorepo.
+
 import lnk from 'lnk';
 import fs from 'fs';
-import path from 'path';
 
+
+// Ensure a clean dist folder
 if (fs.existsSync('dist')) {
     fs.rmSync('dist', { recursive: true, force: true });
 }
 fs.mkdirSync('dist', { recursive: true });
+
+
+// Create links to the contents of lib
 fs.readdir('lib', (error, files) => {
-   files.forEach(file => {
-       fs.stat(path.join('lib', file), (error, stat) => {
-           //console.log(file, stat.isFile());
+    if (error) {
+        console.error(error);
+    } else {
+       files.forEach(file => {
            lnk(`lib/${file}`,'dist');
        });
-   });
+    }
 });
-fs.writeFile('dist/piveau-hub-ui-modules.css', '/* Just a placeholder file in development mode */', error => {
-    if (error) console.log(error);
+
+// Add a placeholder css file
+const fileContent = '/* Just a placeholder file in development mode */';
+fs.writeFile('dist/piveau-hub-ui-modules.css', fileContent, error => {
+    if (error) console.error(error);
 });
