05e08284efb698afc6f6c86d32e455b9a72af0fc
Michael Peters <michael.peters@fokus.fraunhofer.de>
2023-06-16 13:07:12 +0200

    Fixed Annif and mandatory field highlighting

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/utils/translation-helper.js
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/utils/translation-helper.js
@@ -8,6 +8,7 @@ import { has } from 'lodash-es';
  * @param {String} property String defining which property translation should be used
  */
 function translateProperty(propertyDefinition, property) {
+    console.log(propertyDefinition.mandatory);
     if (has(propertyDefinition, 'identifier')) { // hidden fields don't need a label and have no identifier 
         const translatableParameters = ['label', 'info', 'help', 'placeholder', 'add-label'];
         const propertyName = propertyDefinition.identifier;
@@ -29,7 +30,7 @@ function translateProperty(propertyDefinition, property) {
             }
 
             // Highlight mandatory fields
-            if ((propertyDefinition.identifier === 'datasetID' || (has(propertyDefinition, 'validation') && propertyDefinition.validation === 'required')) && parameter === 'label') propertyDefinition[parameter] = `${translation}*`
+            if (propertyDefinition.mandatory) propertyDefinition[parameter] = `${translation}*`
         }
     }
 }
