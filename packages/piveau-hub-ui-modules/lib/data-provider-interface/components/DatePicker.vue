cea9ff02ee3c2c405166760274e234ab749cbc72
Michael Peters <michael.peters@fokus.fraunhofer.de>
2023-07-13 09:18:54 +0200

    fixed modified and issued

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/components/DatePicker.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/components/DatePicker.vue
@@ -60,14 +60,13 @@ export default {
   computed: {
   },
   mounted(){
-    setTimeout(() => {
-      if (this.context.attributes.handler === "issued") this.validateIssued(this.context.model);
-    }, 1500);
+   
     
   },
   methods: {
     onInput() {
       this.context.rootEmit('change');
+      if (this.context.attributes.handler === "issued") this.validateIssued(this.context.model);
     },
     newField() {
       if (this.context.attributes.handler === "modified") this.compareWithIssued(this.context.model);
@@ -94,17 +93,20 @@ export default {
       } catch (error) {
 
       }
-      return date < today || date < issued || date > new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+      if(this.context.attributes.handler === "modified") return date < today || date < issued || date > new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+      else return
     },
     validateIssued(date) {
       const today = new Date();
-      today.setHours(0, 0, 0, 0);
       
-      // if (new Date(date) < today) {
-      //   alert("The date of issued lies before the current date, is that intended?")
-      // }
-
-      return date < today
+      
+      if (new Date(date) < today + 1) {
+        alert("The date of issued lies before the current date, is that intended?")
+      }
+      if (new Date(date) > today - 1) {
+        alert("The date of issued lies after the current date, is that intended?")
+      }
+      
     }
     // compareStartEnd(date) {
 
