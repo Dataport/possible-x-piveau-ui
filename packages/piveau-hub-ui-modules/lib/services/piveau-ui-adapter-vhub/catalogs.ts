3f32e9029abb36b2bb7f0f9a93b42a33f7667ab4
Torben Jastrow <torben.jastrow@fokus.fraunhofer.de>
2023-07-31 08:27:03 +0200

    fix: catalogue page add key

--- packages/piveau-hub-ui-modules/lib/services/piveau-ui-adapter-vhub/catalogs.ts
+++ packages/piveau-hub-ui-modules/lib/services/piveau-ui-adapter-vhub/catalogs.ts
@@ -27,7 +27,7 @@ const getResponseData = (catalog) => {
   cat.languages = dataGetters.getArrayOfObjects(catalog, 'language', ['id', 'label', 'resource']);
   cat.licence = dataGetters.getObject(catalog, 'license', ['id', 'label', 'description', 'resource', 'la_url']);
   cat.modified = dataGetters.getString(catalog, 'modified');
-  cat.publisher = dataGetters.getObject(catalog, 'publisher', ['name', 'type', 'email', 'resource', 'homepage']);
+  cat.publisher = dataGetters.getObject(catalog, 'publisher', ['name', 'type', 'email', 'resource', 'homepage', 'address']);
   cat.record = dataGetters.getObject(catalog, 'catalog_record', ['issued', 'modified']);
   cat.rights = dataGetters.getObject(catalog, 'rights', ['label', 'resource']);
   cat.issued = dataGetters.getString(catalog, 'issued');
