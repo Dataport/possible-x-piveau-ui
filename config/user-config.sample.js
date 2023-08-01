3cbcdc089f14ae8248dbc83849a1b9474b713054
sor67304 <soroush.nikjou@fokus.fraunhofer.de>
2023-08-01 10:33:25 +0200

    replaced 'piveau hub-ui' hardcoded value. Ref: https://gitlab.fokus.fraunhofer.de/piveau/organisation/piveau-scrum-board/-/issues/2823

--- config/user-config.sample.js
+++ config/user-config.sample.js
@@ -85,7 +85,7 @@ const glueConfig = {
     },
   },
   metadata: {
-    title: 'piveau Hub-UI',
+    title: process.env.VUE_APP_METADATA_TITLE,
     description: 'A modern and customizable web application for data management of extensive data catalogs.',
     keywords: 'Open Data',
   },
