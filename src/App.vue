59732da2e519d875452271c3e1fec999ae860fef
sor67304 <soroush.nikjou@fokus.fraunhofer.de>
2023-08-01 09:50:42 +0200

    replaced hardcoded title 'Piveau Hub-ui" with env variable. Ref: https://gitlab.fokus.fraunhofer.de/piveau/organisation/piveau-scrum-board/-/issues/2823

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@ export default {
         return chunk ? `${chunk} - ${this.$env.metadata.title}` : this.$env.metadata.title;
       },
       meta: [
-        { name: 'description', vmid: 'description', content: 'piveau Hub-UI' },
+        { name: 'description', vmid: 'description', content: process.env.VUE_APP_METADATA_TITLE },
         { name: 'keywords', vmid: 'keywords', content: this.$env.metadata.keywords },
       ],
       htmlAttrs: {
