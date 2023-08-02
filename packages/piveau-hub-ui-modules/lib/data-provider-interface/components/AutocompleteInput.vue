c99e69a9bbf8fab576d6ac5377ab20c71bb1e839
Michael Peters <michael.peters@fokus.fraunhofer.de>
2023-08-02 11:52:56 +0200

    styling fixes

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/components/AutocompleteInput.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/components/AutocompleteInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="`formulate-input-element formulate-input-element--${context.type}`" :data-type="context.type"
     v-on="$listeners">
-    <h1>Hallo</h1>
+    
     <div class="input-group suggestion-input-group mb-3" v-click-outside="hideSuggestions">
       <input v-model="context.model" @blur="context.blurHandler" hidden />
       <div class="annifButtonWrap" v-if="annifTheme && annifEnv">
