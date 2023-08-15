47de71f76373adf00dc46c24ff0ba6ef5ca0f2ad
Torben Jastrow <torben.jastrow@fokus.fraunhofer.de>
2023-08-15 11:28:30 +0200

    fix: code styling

--- packages/piveau-hub-ui-modules/lib/catalogues/CatalogPage.vue
+++ packages/piveau-hub-ui-modules/lib/catalogues/CatalogPage.vue
@@ -9,7 +9,8 @@
       <slot name="header">
         <div class="catalog-header-container mx-auto d-flex justify-content-between align-items-center" ref="catHeader">
           <div class="catalog-header-info d-flex">
-            <img v-if="showArray(getCatalog.catalogueFavIcon)" class="catalog-header-title-icon" :src="getCatalog.catalogueFavIcon" alt="">
+            <img v-if="showArray(getCatalog.catalogueFavIcon)" class="catalog-header-title-icon"
+                 :src="getCatalog.catalogueFavIcon" alt="">
             <div class="catalog-header-title-text">
               <h1 class="catalog-header-title-title" aria-label="Katalogtitel">
                 {{ getTranslationFor(getCatalog.title, $route.query.locale, getCatalog.languages) }}
@@ -21,16 +22,19 @@
               </h5>
             </div>
           </div>
-          <img v-if="showArray(getCatalog.catalogueLogo)" class="catalog-header-logo d-none d-sm-block" :src="getCatalog.catalogueLogo" alt="" aria-label="logo">
+          <img v-if="showArray(getCatalog.catalogueLogo)" class="catalog-header-logo d-none d-sm-block"
+               :src="getCatalog.catalogueLogo" alt="" aria-label="logo">
         </div>
       </slot>
 
-      <div class="catalog-card card" >
+      <div class="catalog-card card">
         <div class="card-header" role="navigation">
           <!-- CARD NAV -->
           <ul class="nav nav-tabs card-header-tabs" role="tablist" aria-label="Navigationsleiste">
-            <li v-for="tab in cardNavTabs" :key="tab.id" role="tab" :aria-controls="tab.id" :aria-selected="activeTabName === tab.id" :aria-label="tab.displayName">
-              <a @click.prevent="setActiveTabName(tab.id)" class="catalog-nav-link nav-link" :class="{active: tab.id === activeTabName}" :href="`#${tab.id}`" role="tab">
+            <li v-for="tab in cardNavTabs" :key="tab.id" role="tab" :aria-controls="tab.id"
+                :aria-selected="activeTabName === tab.id" :aria-label="tab.displayName">
+              <a @click.prevent="setActiveTabName(tab.id)" class="catalog-nav-link nav-link"
+                 :class="{active: tab.id === activeTabName}" :href="`#${tab.id}`" role="tab">
                 {{ tab.displayName }}
               </a>
             </li>
@@ -40,34 +44,41 @@
         <div class="card-body my-5">
 
           <!-- "ÜBER DIESEN KATALOG" -->
-          <div v-if="activeTabName === 'about'" class="tab-pane active catalog-about-tab d-flex align-items-start justify-content-between" id="about" role="tabpanel">
+          <div v-if="activeTabName === 'about'"
+               class="tab-pane active catalog-about-tab d-flex align-items-start justify-content-between" id="about"
+               role="tabpanel">
             <div>
               <div class="tab-content d-flex">
-                <app-markdown-content class="card-text" tag="div" :text="getTranslationFor(catalog.description, $route.query.locale, catalog.languages)" />
+                <app-markdown-content class="card-text" tag="div"
+                                      :text="getTranslationFor(catalog.description, $route.query.locale, catalog.languages)"/>
               </div>
             </div>
-            <img v-if="showArray(getCatalog.catalogueProfile)" class="catalog-hero-pic img-fluid" :src="getCatalog.catalogueProfile" alt="">
+            <img v-if="showArray(getCatalog.catalogueProfile)" class="catalog-hero-pic img-fluid"
+                 :src="getCatalog.catalogueProfile" alt="">
           </div>
 
           <!-- "INTERESSANTE DATENSÄTZE" -->
-          <div v-show="activeTabName === 'dataset-selections'" class="tab-pane active" id="dataset-selections" role="tabpanel" aria-labelledby="dataset-selections-tab">
+          <div v-show="activeTabName === 'dataset-selections'" class="tab-pane active" id="dataset-selections"
+               role="tabpanel" aria-labelledby="dataset-selections-tab">
             <div v-if="showArray(getDatasets)" class="tab-content d-flex flex-column align-items-center mt-3">
               <div class="d-flex flex-wrap justify-content-center mb-5">
 
                 <DatasetCard
-                  v-for="dataset in (interestingDatasets.length > 0) ? interestingDatasets.slice(0, 3) : getDatasets.slice(0, 3)"
-                  :key="dataset.id"
-                  :to="`/datasets/${dataset.id}`"
-                  :datasetTitle="getTranslationFor(dataset.title, $route.query.locale, dataset.languages) || dataset.id"
-                  :datasetDescription="getTranslationFor(dataset.description, $route.query.locale, dataset.languages)"
-                  :datasetCatalog="getTranslationFor(dataset.catalog.title, $route.query.locale, [])"
-                  :descriptionMaxLength="175"
-                  :data-cy="`dataset@${dataset.id}`"
-                  class="mt-3"
+                    v-for="dataset in (interestingDatasets.length > 0) ? interestingDatasets.slice(0, 3) : getDatasets.slice(0, 3)"
+                    :key="dataset.id"
+                    :to="`/datasets/${dataset.id}`"
+                    :datasetTitle="getTranslationFor(dataset.title, $route.query.locale, dataset.languages) || dataset.id"
+                    :datasetDescription="getTranslationFor(dataset.description, $route.query.locale, dataset.languages)"
+                    :datasetCatalog="getTranslationFor(dataset.catalog.title, $route.query.locale, [])"
+                    :descriptionMaxLength="175"
+                    :data-cy="`dataset@${dataset.id}`"
+                    class="mt-3"
                 />
               </div>
 
-              <button @click.prevent="setActiveTabName('dataset-page')" class="fol-button primary-button" role="button">Mehr Daten entdecken</button>
+              <button @click.prevent="setActiveTabName('dataset-page')" class="fol-button primary-button" role="button">
+                Mehr Daten entdecken
+              </button>
             </div>
 
             <span v-else>
@@ -76,41 +87,55 @@
           </div>
 
           <!-- "ALLE DATENSÄTZE" -->
-          <div v-show="activeTabName === 'dataset-page'" class="tab-pane active" id="dataset-page" role="tabpanel" aria-labelledby="dataset-page-tab">
+          <div v-show="activeTabName === 'dataset-page'" class="tab-pane active" id="dataset-page" role="tabpanel"
+               aria-labelledby="dataset-page-tab">
             <slot name="card-datasets">
               <div class="tab-content mt-3">
-                <Datasets />
+                <Datasets/>
               </div>
             </slot>
           </div>
 
           <!-- "KONTAKT" -->
-          <div v-show="activeTabName === 'contact'" class="tab-pane active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+          <div v-show="activeTabName === 'contact'" class="tab-pane active" id="contact" role="tabpanel"
+               aria-labelledby="contact-tab">
             <div class="tab-content">
               <dl v-if="has(getCatalog, 'publisher') && showObject(getCatalog.publisher) && (showString(getCatalog.publisher.email) || showString(getCatalog.publisher.homepage) || showObject(getCatalog.publisher.address))">
                 <h4 class="catalog-contact-header">Fragen zu dieser Open Data Präsenz?</h4>
                 <!-- publisher name -->
-                <dt v-if="has(getCatalog, 'publisher.name')  && showString(getCatalog.publisher.name)">{{ getCatalog.publisher.name }}</dt>
+                <dt v-if="has(getCatalog, 'publisher.name')  && showString(getCatalog.publisher.name)">
+                  {{ getCatalog.publisher.name }}
+                </dt>
                 <!-- address -->
                 <dd v-if="has(getCatalog, 'publisher.address') && showObject(getCatalog.publisher.address)">
                   <!-- E-Mail:  -->
-                  <span v-if="has(getCatalog, 'publisher.address.street') && showString(getCatalog.publisher.address.street)">{{ getCatalog.publisher.address.street }}</span>
+                  <span
+                      v-if="has(getCatalog, 'publisher.address.street') && showString(getCatalog.publisher.address.street)">{{
+                      getCatalog.publisher.address.street
+                    }}</span>
                 </dd>
                 <dd v-if="has(getCatalog, 'publisher.address') && showObject(getCatalog.publisher.address)">
                   <!-- E-Mail:  -->
-                  <span v-if="has(getCatalog, 'publisher.address.postalCode') && showString(getCatalog.publisher.address.locality) && has(getCatalog, 'publisher.address.locality') && showString(getCatalog.publisher.address.postalCode)">{{ getCatalog.publisher.address.postalCode }} {{ getCatalog.publisher.address.locality }}</span>
+                  <span
+                      v-if="has(getCatalog, 'publisher.address.postalCode') && showString(getCatalog.publisher.address.locality) && has(getCatalog, 'publisher.address.locality') && showString(getCatalog.publisher.address.postalCode)">{{
+                      getCatalog.publisher.address.postalCode
+                    }} {{ getCatalog.publisher.address.locality }}</span>
                 </dd>
                 <!-- email -->
                 <dd>
                   <!-- E-Mail:  -->
-                  <app-link v-if="has(getCatalog, 'publisher.email') && showString(getCatalog.publisher.email)" :to="getCatalog.publisher.email">
-                    {{ getCatalog.publisher.email.startsWith("mailto:") ? getCatalog.publisher.email.slice(7) : getCatalog.publisher.email }}
+                  <app-link v-if="has(getCatalog, 'publisher.email') && showString(getCatalog.publisher.email)"
+                            :to="getCatalog.publisher.email">
+                    {{
+                      getCatalog.publisher.email.startsWith("mailto:") ? getCatalog.publisher.email.slice(7) : getCatalog.publisher.email
+                    }}
                   </app-link>
                 </dd>
                 <!-- homepage -->
                 <dd>
                   <!-- Website:  -->
-                  <app-link v-if="has(getCatalog, 'publisher.homepage') && showString(getCatalog.publisher.homepage)" :to="getCatalog.publisher.homepage">
+                  <app-link v-if="has(getCatalog, 'publisher.homepage') && showString(getCatalog.publisher.homepage)"
+                            :to="getCatalog.publisher.homepage">
                     {{ getCatalog.publisher.homepage }}
                   </app-link>
                 </dd>
@@ -128,7 +153,7 @@
 
 <script>
 import Datasets from "../datasets/Datasets.vue"
-import { mapGetters, mapActions } from 'vuex';
+import {mapGetters, mapActions} from 'vuex';
 import AppLink from "../widgets/AppLink";
 import DatasetCard from "./CatalogPageDatasetCard.vue"
 import AppMarkdownContent from "../datasetDetails/AppMarkdownContent.vue"
@@ -139,7 +164,8 @@ import {
   isObject,
   isString
 } from 'lodash-es';
-import { getTranslationFor, truncate } from '../utils/helpers';
+import {getTranslationFor, truncate} from '../utils/helpers';
+
 export default {
   name: "CatalogPage",
   dependencies: ['catalogService', 'DatasetService'],
@@ -154,34 +180,34 @@ export default {
       interestingDatasets: [],
       activeTabName: 'about',
       cardNavTabs: [
-      {
-        id: 'about',
-        displayName: 'Über diesen Katalog',
-      },
-      {
-        id: 'dataset-selections',
-        displayName: 'Interessante Datensätze',
-      },
-      {
-        id: 'dataset-page',
-        displayName: 'Alle Datensätze',
-      },
-      {
-        id: 'contact',
-        displayName: 'Kontakt',
-      },
+        {
+          id: 'about',
+          displayName: 'Über diesen Katalog',
+        },
+        {
+          id: 'dataset-selections',
+          displayName: 'Interessante Datensätze',
+        },
+        {
+          id: 'dataset-page',
+          displayName: 'Alle Datensätze',
+        },
+        {
+          id: 'contact',
+          displayName: 'Kontakt',
+        },
       ],
     };
   },
   computed: {
     ...mapGetters('catalogDetails', [
-    'getCatalog',
+      'getCatalog',
     ]),
     ...mapGetters('datasets', [
-    'getDatasets',
-    'getDatasetsCount',
-    'getLoading',
-    'getOffset',
+      'getDatasets',
+      'getDatasetsCount',
+      'getLoading',
+      'getOffset',
     ]),
     catalog() {
       return this.getCatalog;
@@ -195,8 +221,8 @@ export default {
     truncate,
     getTranslationFor,
     ...mapActions('catalogDetails', [
-    'loadCatalog',
-    'useCatalogService',
+      'loadCatalog',
+      'useCatalogService',
     ]),
     ...mapActions('datasets', [
       'loadDatasets',
@@ -223,10 +249,10 @@ export default {
     loadCatalogInterestingDatasets(catalog) {
       for (let id of this.catalog.catalogueInterestingDatasets) {
         this.DatasetService.getSingle(id)
-          .then(response => {
-            this.interestingDatasets.push(response);
-          })
-          .catch(error => console.log('error: ', error));
+            .then(response => {
+              this.interestingDatasets.push(response);
+            })
+            .catch(error => console.log('error: ', error));
       }
     }
   },
@@ -412,6 +438,7 @@ export default {
 .tab-content dt {
   margin-bottom: 8px;
 }
+
 .tab-content dd {
   margin-bottom: 0;
 }
