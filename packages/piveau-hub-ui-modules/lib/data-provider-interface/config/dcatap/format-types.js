519996db3beb652cf12952873c6de8b299bba1cb
Bianca Wentzel <bianca.wentzel@fokus.fraunhofer.de>
2023-08-25 13:11:58 +0200

    fixed handling of spatial metadata and display in form

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/config/dcatap/format-types.js
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/config/dcatap/format-types.js
@@ -191,6 +191,7 @@ const multiURIobjects = {
         "prov:wasGeneratedBy",
         "dct:isVersionOf",
         "dct:hasVersion",
+        "dct:spatial",
     ],
     distributions: [
         "dcat:accessURL",
