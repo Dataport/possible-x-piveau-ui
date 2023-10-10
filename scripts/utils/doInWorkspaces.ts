ce91355d8da363f8ab5aa1e9e63361f7e4f57b27
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2023-10-10 11:30:55 +0200

    correction initialize script

--- scripts/utils/doInWorkspaces.ts
+++ scripts/utils/doInWorkspaces.ts
@@ -2,6 +2,10 @@ import fs, {Stats} from "fs";
 import {readFile} from "fs/promises";
 import {doForFoldersInFolder, FilesAction} from "./doInFolder";
 
+/**
+ * Look into the root package.json for all workspaces and execute a given action in all of them
+ * @param action
+ */
 export const doInWorkspaces = async (action: FilesAction) => {
     const workspaces: string[] = JSON.parse(await readFile("./package.json", 'utf-8')).workspaces;
     workspaces.forEach(workspace => {
