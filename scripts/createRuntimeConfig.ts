ef271eb91e2d167cb63ef88c60d641409e6ed0cd
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2023-10-27 12:51:16 +0200

    improve createRuntimeConfig.ts

--- scripts/createRuntimeConfig.ts
+++ scripts/createRuntimeConfig.ts
@@ -1,109 +1,93 @@
 import fs, {Stats} from "fs";
-// import {doForAppConfig} from "./utils/doForAppConfig";
-import { readFile } from 'fs/promises';
-// import esprima from 'esprima';
-
-const app = process.argv[2] || 'modules-demo';
-
-export const createRuntimeConfig = async (app: string, appStats: Stats, folder: string) => {
-    const userConfigSample = `./${folder}/${app}/config/user-config.sample.js`;
-    // import(userConfigSample).then(module => {
-    //     console.log("YEA", module);
-    // }).catch((error) => console.log("Error!", error));
-    const content = await readFile(userConfigSample, 'utf-8');
-    const glueConfigAssignment = (content.substring(content.indexOf("glueConfig")));
-    const glueConfigContent = (glueConfigAssignment.substring(glueConfigAssignment.indexOf("{")));
-    // return esprima.tokenize(glueConfigAssignment)
-    // return eval(glueConfigContent)
-    // return glueConfigContent;
-    // const uncommented = glueConfigContent.replace(/[^:]\/{2}.*\n/g, "");
-    // const quotedGlueConfigContent = uncommented.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
-    // return JSON.parse(quotedGlueConfigContent);
-    // return quotedGlueConfigContent.substring(35, 100)
-};
-
-// doForAppConfig(app, (app: string, stats: Stats, folder: string) => {
-//     createRuntimeConfig(app, stats, folder).then(result => {
-//         console.log("RESULT", result)
-//     });
-// });
 
 import {configSchema} from "../packages/piveau-hub-ui-modules/lib/configurations/config-schema";
-import {ZodObject} from 'zod'
-// console.log("configSchema", configSchema instanceof ZodObject)
+import {ZodObject, ZodType, ZodDefault, ZodEffects} from 'zod';
+import {pathByWorkspaceName} from "./utils/pathByWorkspaceName";
+import {doForApps} from "./utils/doForApp";
 
-const values = configSchema.keyof()._def.values
-// console.log("configSchema2", Object.keys(getKeys(configSchema)), )
-// console.log("configSchema2", values, configSchema.keyof()._def.values)
-
-function iterate(z: ZodObject<any>, pad='') {
-    const entries = z instanceof ZodObject ? z.shape : z;
-    for (const [key, value] of Object.entries(entries)) {
-        console.log(pad, key, value.constructor.name);
-        if (key === 'content') {
-            // console.log(Object.entries(value))
-            // console.log(value.constructor.name)
-            iterate(value, '>>>');
-        }
+/**
+ * Turns a Zod object into a regular Javascript key-value object where the values are still Zod objects
+ * @param z
+ */
+function getShape(z: ZodType) {
+    if (z instanceof ZodDefault) {
+        return getShape(z._def.innerType);
+    } else if (z instanceof ZodEffects) {
+        return getShape(z._def.schema);
+    } else if (z instanceof ZodObject) {
+        return z.shape;
+    } else {
+        return null;
     }
-}
-
-iterate(configSchema)
-
-// Assuming we start in content at given position with a curly brace, indicating a stringified Javascript object,
-// parses through that string and returns an object containing the same keys and string values coding
-// the nested keys.
-const transform = (content: string, position: number, prefix: string) => {
-    ++position; // Get past '{'
-    let next, char, valueRead;
-    const result = {};
-    while (true) {
-        next = content.search(/\w}]/);
-        char = content.charAt(position);
-        if (char === '}') {
-            return { result, position };
-        } else if (/\w/.test(char)) {
-            const end = content.substring(next).search(/\W/);
-            const key = content.substring(next, end);
-            position = end;
-            const colon = content.substring(position).search(/:/);
-            if (colon === -1) {
-                console.error(`No colon after key found at position ${position}!`);
-                break;
-            } else {
-                ++position; // Get past ':'
-                valueRead = false;
-                // TODO
-                // do {
-                //     next = content.substring(position).search(/\S/); // Next non whitespace
-                //     char = content.charAt(next);
-                //     if (char === '\\' && content.charAt(next + 1) === '\\') { // Get past a comment
-                //
-                //     }
-                //     if (char === '\'') {
-                //         valueRead = true;
-                //     } else if (char === '"') {
-                //         valueRead = true;
-                //     } else if (char === '{') {
-                //
-                //     }
-                // }
-            }
-        } else {
-            console.error(`Expected a key at between positions ${position} and ${next}!`);
-            break;
-        }
-    }
-    return {}
 }
 
 /**
- * Starting past the opening curly braces, using a tokenization array
- * @param prefix
+ * Recursively turns a Zod object into a regular Javascript key-value object, where the values
+ * are strings that have the special shape to start with "$VUE_APP_" and then chaining the keys
+ * path in upper case separated by underscores.
+ * @param z
+ * @param keyPath
  */
-function readConfigObject(tokenization: [{type: string, value: string}], tokenPosition: number, prefix: string) {
-    const result = {};
-    while (true) {
-        const identifer = tokenPosition[tokenization].value;
+function convert(z: ZodType, keyPath: string = '') {
+    const entries = getShape(z);
+    if (entries instanceof Object) {
+        for (const [key, value] of Object.entries(entries)) {
+            const extendedKeyPath = keyPath === '' ? key.toUpperCase() : keyPath + '_' + key.toUpperCase();
+            const convertedValue = convert(value as ZodType, extendedKeyPath);
+            if (convertedValue === null) {
+                entries[key] = "$VUE_APP_" + keyPath + "_"
+                    + key.replace(/[A-Z]+(?![a-z])|[A-Z]/g, (sub, arg) => (arg ? "_" : "") + sub).toUpperCase();
+            } else {
+                entries[key] = convertedValue;
+            }
+        }
+        return entries;
+    } else {
+        return null;
     }
 }
+
+// const runtimeConfig = convert(configSchema);
+// console.log(JSON.stringify(runtimeConfig, null, 2));
+
+function runtimeConfigAsString() {
+    const fileStart = `/**
+ * Configuration template file to bind specific properties from user-config.js to environment variables.
+ *
+ * This object MUST be structurally identical to the standard user-config.sample.js file.
+ * Each value MUST start with the $VUE_APP_ prefix and SHOULD be followed by their path, e.g.
+ *
+ * api: { baseUrl: '$VUE_APP_API_BASE_URL' }
+ *
+ * Their corresponding environment variable keys MUST equal that value without the $ character, e.g.
+ *
+ * VUE_APP_API_BASE_URL: '/base'
+ */
+ 
+export default `;
+
+    const fileBody = JSON.stringify(convert(configSchema), null, 2);
+
+    const fileEnd = `;`;
+
+    return fileStart + fileBody + fileEnd;
+}
+
+export function writeRuntimeConfig(filePath: string) {
+    fs.writeFileSync(filePath, runtimeConfigAsString());
+    console.log("Created", filePath);
+}
+
+export function createRuntimeConfig(workspaceName) {
+    if (workspaceName) {
+        pathByWorkspaceName(workspaceName).then((filePath) => {
+            writeRuntimeConfig(`${filePath}/config/runtime-config.js`);
+        })
+    } else {
+        doForApps((file: string, stats: Stats, folder: string) => {
+            writeRuntimeConfig(`${folder}/${file}/config/runtime-config.js`);
+        });
+    }
+}
+
+createRuntimeConfig(process.argv[2]);
