11347452221935f17de33c9eafe2a2387545ffd0
Duy Minh Vo <duy.minh.vo@fokus.fraunhofer.de>
2024-01-11 12:16:53 +0100

    feat(DistributionInputPage): implement support for multiple distributions

---
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/components/DistributionStepper.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <div name="distribution-stepper">
+    <h3 v-if="name">{{ name }}</h3>
+    <div class="disInputInteractionButtons">
+      <div v-for="step, index in distributionSteps" :key="step">
+        <span v-on:click="distributionSteps[index] = !distributionSteps[index]">Step {{ index + 1 }}</span>
+      </div>
+    </div>
+    <FormKit type="group">
+      <div v-for="step, index in distributionSteps" :key="step">
+        <div v-show="distributionSteps[index]">
+          <FormKitSchema :schema="schema[index]" />
+        </div>
+      </div>
+    </FormKit>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    schema: {
+      required: true,
+      type: Object,
+    }
+  }, 
+  data() {
+    return {
+      distributionSteps: [
+        false,
+        false,
+        false,
+        false
+      ],
+    }
+  },
+});
+</script>
+
+<style></style>
--- packages/piveau-hub-ui-modules/lib/data-provider-interface/views/DistributionInputPage.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/views/DistributionInputPage.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="disInputInteractionButtons">
-    <div v-for="step, index in distributionSteps" :key="step">
-      <span v-on:click="distributionSteps[index] = !distributionSteps[index]">Step {{ index + 1 }}</span>
-    </div>
-  </div>
-  <div v-for="step, index in distributionSteps" :key="step">
-    <div v-show="distributionSteps[index]">
-      <FormKitSchema :schema="schema[index]" />
-    </div>
+  <div name="distribution-stepper-list">
+    <FormKit type="list" name="distributionList">
+      <section v-for="i in distributionList" :key="i">
+        <DistributionStepper :name="`Distribution ${i}`" :schema="schema" />
+        <hr />
+      </section>
+    </FormKit>
+    <button class="btn btn-secondary" @click="pushDistribution">Add Distribution</button>
+    <button class="btn btn-secondary" @click="popDistribution">Remove Distribution</button>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import DistributionStepper from '../components/DistributionStepper.vue';
 
 export default defineComponent({
   props: {
@@ -20,16 +21,30 @@ export default defineComponent({
       required: true,
       type: Object,
     }
-  }, 
+  },
+  components: {
+    DistributionStepper,
+  },
   data() {
     return {
-      distributionSteps: [
-        false,
-        false,
-        false,
-        false
-      ],
+      latestNonce: 0,
+      distributionList: [],
     }
+  },
+  created() {
+    this.pushDistribution();
+  },
+  methods: {
+    pushDistribution() {
+      this.distributionList.push(this.latestNonce);
+      this.latestNonce++;
+    },
+    popDistribution() {
+      this.distributionList.pop();
+    },
+    removeDistributionByIndex(index) {
+      this.distributionList.splice(index, 1);
+    },
   },
 });
 </script>
