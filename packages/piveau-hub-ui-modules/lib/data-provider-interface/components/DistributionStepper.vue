9fdda707776b421ae3fcdae01d30ad7e48db8732
Duy Minh Vo <duy.minh.vo@fokus.fraunhofer.de>
2024-02-20 15:03:27 +0100

    fix(DistributionStepper): wrap content around FormKit Form type

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/components/DistributionStepper.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/components/DistributionStepper.vue
@@ -2,64 +2,57 @@
 
 </script>
 <template>
-  <div name="distribution-stepper">
-    <h3 v-if="name">{{ name }}</h3>
-
-    <div class="d-flex">
-      <ul class="steps">
-        <li v-for="(step, stepName, index) in steps" :key="index" 
-          class="step" :data-step-active="activeStep === stepName" :data-step-valid="step.valid && step.errorCount === 0"
-          :class="{ 
-            activeItem: activeStep === stepName, inactiveStep: stepName != activeStep, 'has-errors': checkStepValidity(stepName)
-          }" 
-          @click="activeStep = stepName">
-
-
-          <div class="stepBubbleWrap">
-            <div class="circle stepCircle">{{ index + 1 }}</div>
-            <span v-if="checkStepValidity(stepName)" class="step--errors"/>
-              <!-- v-text="step.errorCount + step.blockingCount" /> -->
-            {{ camel2title(stepName) }}
+  <FormKit type="form" v-model.lazy="formValues" :actions="false" :plugins="[stepPlugin]">
+    <div name="distribution-stepper">
+      <h3 v-if="name">{{ name }}</h3>
+      <div class="d-flex">
+        <ul class="steps">
+          <li v-for="(step, stepName, index) in steps" :key="index"
+            class="step" :data-step-active="activeStep === stepName" :data-step-valid="step.valid && step.errorCount === 0"
+            :class="{
+              activeItem: activeStep === stepName, inactiveStep: stepName != activeStep, 'has-errors': checkStepValidity(stepName)
+            }"
+            @click="activeStep = stepName">
+            <div class="stepBubbleWrap">
+              <div class="circle stepCircle">{{ index + 1 }}</div>
+              <span v-if="checkStepValidity(stepName)" class="step--errors"/>
+                <!-- v-text="step.errorCount + step.blockingCount" /> -->
+              {{ camel2title(stepName) }}
+            </div>
+            <div v-if="index + 1 != Object.keys(getNavSteps.distributions).length" class="seperatorHorizontalStepper"></div>
+            <div v-if="activeStep === 'overview'" class="seperatorHorizontalStepper"></div>
+          </li>
+          <li class="step inactiveStep" v-if="activeStep === 'overview'">
+            <div class="circle stepCircle"></div>
+          </li>
+        </ul>
+        <div class="d-flex flex-column w-100">
+          <div v-for="(stepName, index) in getNavSteps.distributions" :key="index">
+            <InputPageStep :name="stepName">
+              <!-- <PropertyChooser></PropertyChooser> -->
+              <FormKitSchema :schema="schema[stepName]"/>
+              <p class="p-1"> <b>*</b> mandatory</p>
+            </InputPageStep>
           </div>
-
-          <div v-if="index + 1 != Object.keys(getNavSteps.distributions).length" class="seperatorHorizontalStepper"></div>
-          <div v-if="activeStep === 'overview'" class="seperatorHorizontalStepper"></div>
-        </li>
-
-        <li class="step inactiveStep" v-if="activeStep === 'overview'">
-          <div class="circle stepCircle"></div>
-        </li>
-
-      </ul>
-
-      <div class="d-flex flex-column w-100">
-        <div v-for="(stepName, index) in getNavSteps.distributions" :key="index">
-          <InputPageStep :name="stepName">
-            <!-- <PropertyChooser></PropertyChooser> -->
-            <FormKitSchema :schema="schema[stepName]"/>
-            <p class="p-1"> <b>*</b> mandatory</p>
-          </InputPageStep>
         </div>
       </div>
-    </div>
-
-    <!-- <Navigation :steps="distSteps" :nextStep="distNextStep" :previousStep="distPreviousStep" 
-            :goToNextStep="distGoToNextStep" :goToPreviousStep="distGoToPreviousStep"></Navigation> -->
-
-    <!-- <h5>{{ getDisName() }}</h5> -->
-    <!-- <div class="disInputInteractionButtons">
-      <div v-for="step, index in distributionSteps" :key="step">
-        <span v-on:click="handleClick(index)"> {{ step.name }}</span>
-      </div>
-    </div>
-    <FormKit type="group">
-      <div v-for="step, index in distributionSteps" :key="step">
-        <div v-show="step['show']">
-          <FormKitSchema :schema="schema[index]" />
+      <!-- <Navigation :steps="distSteps" :nextStep="distNextStep" :previousStep="distPreviousStep"
+              :goToNextStep="distGoToNextStep" :goToPreviousStep="distGoToPreviousStep"></Navigation> -->
+      <!-- <h5>{{ getDisName() }}</h5> -->
+      <!-- <div class="disInputInteractionButtons">
+        <div v-for="step, index in distributionSteps" :key="step">
+          <span v-on:click="handleClick(index)"> {{ step.name }}</span>
         </div>
       </div>
-    </FormKit> -->
-  </div>
+      <FormKit type="group">
+        <div v-for="step, index in distributionSteps" :key="step">
+          <div v-show="step['show']">
+            <FormKitSchema :schema="schema[index]" />
+          </div>
+        </div>
+      </FormKit> -->
+    </div>
+  </FormKit>
 </template>
 
 <script>
