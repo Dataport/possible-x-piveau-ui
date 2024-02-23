e05c046991de6ebc2bc60b7c69dae115b9502a85
mad1 <Nicolas.Matthaes@fokus.fraunhofer.de>
2024-02-23 12:47:19 +0100

    fix: Autocomplete working but throws error in console

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/store/modules/autocompleteStore.ts
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/store/modules/autocompleteStore.ts
@@ -1,24 +1,19 @@
 // @ts-nocheck
 /* eslint-disable no-param-reassign, no-shadow, no-console */
 import axios from 'axios';
-import { getCurrentInstance } from "vue";
 import { inject } from 'vue';
 
-function getEnv() {
-    return getCurrentInstance().appContext.app.config.globalProperties.$env;
-}
-
-function getGlobalEnv() {
+function getEnvironmentVariables() {
     return inject('env');
 }
 
 const state = {};
 const getters = {};
-// ToDo URL is hardcoded, have to get it from the config
+
 const actions = {
     requestFirstEntrySuggestions({ commit }, voc) {
         return new Promise((resolve, reject) => {
-            const req = `${getGlobalEnv().api.baseUrl}search?filter=vocabulary&vocabulary=${voc}&autocomplete=true`;
+            const req = `${getEnvironmentVariables().api.baseUrl}search?filter=vocabulary&vocabulary=${voc}&autocomplete=true`;
             axios.get(req)
             .then((res) => {
                 resolve(res);
@@ -29,14 +24,9 @@ const actions = {
         });
     },
     requestAutocompleteSuggestions({ commit }, { voc, text }) {
-
-        console.log(getEnv().api.baseUrl)
-        console.log(getGlobalEnv().api.baseUrl)
-
-        let url = getGlobalEnv().api.baseUrl; 
         return new Promise((resolve, reject) => {
             const input = text;
-            const req = `${url}search?filter=vocabulary&vocabulary=${voc}&autocomplete=true&q=${input}`;
+            const req = `${getEnvironmentVariables().api.baseUrl}search?filter=vocabulary&vocabulary=${voc}&autocomplete=true&q=${input}`;
             axios.get(req)
             .then((res) => {
                 resolve(res);
@@ -51,16 +41,16 @@ const actions = {
         if(voc === undefined) return
         if(voc === "application") return 
 
-        const dpiConfig = generalDpiConfig[getGlobalEnv().content.dataProviderInterface.specification];   
+        const dpiConfig = generalDpiConfig[getEnvironmentVariables().content.dataProviderInterface.specification];   
         const value = encodeURIComponent(resource.replace(dpiConfig.vocabPrefixes[voc], ""));
         let req;
 
         // vocabularies for spdx checksum and inana-media-types are structured differently in the backend then other vocabularies
         if (voc === 'iana-media-types' || voc === 'spdx-checksum-algorithm') {
-            req = `${getGlobalEnv().api.baseUrl}vocabularies/${voc}`;
+            req = `${getEnvironmentVariables().api.baseUrl}vocabularies/${voc}`;
            
         } else {
-            req = `${getGlobalEnv().api.baseUrl}vocabularies/${voc}/${value}`;
+            req = `${getEnvironmentVariables().api.baseUrl}vocabularies/${voc}/${value}`;
             
         }
         return new Promise((resolve, reject) => {
