b236b3e25662c01c532fa8979a1d3b1602572c4e
mic29226 <michael.peters@fokus.fraunhofer.de>
2024-05-10 16:52:56 +0200

    fixed the URI Prop and the spatial/geocoding input

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/store/modules/autocompleteStore.ts
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/store/modules/autocompleteStore.ts
@@ -6,69 +6,80 @@ import generalDpiConfig from '../../config/dpi-spec-config.js';
 const accesToken = import.meta.env
 
 
-async function getEnvironmentVariables() {    
-    return getCurrentInstance().appContext.app.config.globalProperties.$env; 
-}
+async function getEnvironmentVariables() {
 
+    return getCurrentInstance().appContext.app.config.globalProperties.$env;
+
+}
 const state = {};
 const getters = {};
 
 const actions = {
-    requestFirstEntrySuggestions({ commit }, voc, base) {      
+    requestFirstEntrySuggestions({ commit }, voc, base) {
         return new Promise((resolve, reject) => {
             const req = `${base}search?filter=vocabulary&vocabulary=${voc}`;
             axios.get(req)
-            .then((res) => {
-                resolve(res);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-        });
-    },
-    requestAutocompleteSuggestions({ commit }, { voc, text, base }) {
-        // console.log(voc,text,base); 
-            return new Promise((resolve, reject) => {
-                const req = `${base}search?filter=vocabulary&vocabulary=${voc}&q=${text}`;            
-                axios.get(req)
                 .then((res) => {
-                    // console.log(res);
-                    
-                    resolve(res);         
+                    resolve(res);
                 })
                 .catch((err) => {
                     reject(err);
                 });
-            });
-    },
-    async requestResourceName(voc, uri, conf) {
-        // Catching invalid URI's
-        if(voc === undefined) return
-        if(voc === "application") return 
-
-        console.log(voc, uri);
-        
-        const value = encodeURIComponent(uri.replace(conf.vocabPrefixes[voc], ""));
-        let req;
-
-        // vocabularies for spdx checksum and inana-media-types are structured differently in the backend then other vocabularies
-        if (voc === 'iana-media-types' || voc === 'spdx-checksum-algorithm') {
-            req = `${await getEnvironmentVariables().api.baseUrl}vocabularies/${voc}`;
-           
-        } else {
-            req = `${await getEnvironmentVariables().api.baseUrl}vocabularies/${voc}/${value}`;
-            
-        }
-        return new Promise((resolve, reject) => {
-            axios.get(req)
-            .then((res) => {        
-                resolve(res);
-            })
-            .catch((err) => {             
-                reject(err);
-                 
-            });
         });
+    },
+    requestAutocompleteSuggestions({ commit }, { voc, text, base }) {
+        // console.log(voc,text,base); 
+        return new Promise((resolve, reject) => {
+            const req = `${base}search?filter=vocabulary&vocabulary=${voc}&q=${text}`;
+            axios.get(req)
+                .then((res) => {
+                    // console.log(res);
+
+                    resolve(res);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
+        });
+    },
+    async requestResourceName({ commit }, { voc, uri }) {
+        try {
+            let config = await getCurrentInstance().appContext.app.config.globalProperties.$env
+
+            // console.log(config);
+            // Catching invalid URI's
+
+            if (voc === undefined) return
+            if (voc === "application") return
+
+            // console.log(voc, uri);
+
+            const value = encodeURIComponent(uri.replace(generalDpiConfig.dcatapde.vocabPrefixes[voc], ""));
+            let req;
+
+            // vocabularies for spdx checksum and inana-media-types are structured differently in the backend then other vocabularies
+            if (voc === 'iana-media-types' || voc === 'spdx-checksum-algorithm') {
+                req = `${await config.api.baseUrl}vocabularies/${voc}`;
+
+            } else {
+                req = `${await config.api.baseUrl}vocabularies/${voc}/${value}`;
+
+            }
+            return new Promise((resolve, reject) => {
+                axios.get(req)
+                    .then((res) => {
+                        resolve(res);
+                    })
+                    .catch((err) => {
+                        reject(err);
+
+                    });
+            });
+        } catch (error) {
+            // console.log(error);
+
+        }
+
     },
 };
 
--- packages/piveau-hub-ui-modules/lib/data-provider-interface/views/OverviewPage/Properties/URIProp.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/views/OverviewPage/Properties/URIProp.vue
@@ -6,28 +6,17 @@
 
   <!-- MULTIPLE URIs -->
   <td v-if="value.type === 'multiURI'" class="flex-wrap d-flex multiURI">
-    <!-- <details>{{namesOfMulti}}</details> -->
     <div v-for="(el, index) in namesOfMulti" :key="index" class="border shadow-sm p-2 mb-1 mr-1">
       {{ el['name'] }}
     </div>
   </td>
-  <!-- political geocoding URI -->
-  <td v-if="value.type === 'multiURIspecial'">
-    <div v-for="(el, index) in data[property]" :key="index">
-      {{ el['dcatde:politicalGeocodingURI'] }}
-    </div>
-  </td>
-  <!-- Spatial -->
-  <td v-if="value.type === 'multiURISpatial'">
-    <div v-for="(el, index) in data[property]" :key="index">
-      {{ el['name'] }}
-    </div>
-  </td>
+  
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 import { getTranslationFor } from "../../../../utils/helpers";
+
 export default {
   data() {
     return {
@@ -43,7 +32,6 @@ export default {
   },
   created() {
     try {
-      // console.log(typeof this.data[this.property], 'type', this.data[this.property]);
       if (this.data[this.property]) {
         this.displayURIName(this.value.voc, this.data[this.property]['resource'])
       }
@@ -64,59 +52,73 @@ export default {
       "requestResourceName",
     ]),
     getTranslationFor,
-    // Try to avoid that the function gets called whenever the page gets changed - performance wise this is not optimal
+    async getUriName(voc, res) {
+
+      if (res != undefined) {
+        let vocMatch =
+          this.voc === "iana-media-types" ||
+          this.voc === "spdx-checksum-algorithm";
+
+        let name;
+        await this.requestResourceName({ voc: voc, uri: res }).then(
+          (response) => {
+            if (this.property === 'dcatde:politicalGeocodingURI') {
+              let result = vocMatch
+                ? response.data.result.results
+                  .filter((dataset) => dataset.resource === res)
+                  .map((dataset) => dataset.alt_label)[0].en
+                : getTranslationFor(response.data.result.alt_label, this.$i18n.locale, []);
+              name = result;
+            } else {
+              let result = vocMatch
+                ? response.data.result.results
+                  .filter((dataset) => dataset.resource === res)
+                  .map((dataset) => dataset.pref_label)[0].en
+                : getTranslationFor(response.data.result.pref_label, this.$i18n.locale, []);
+              name = result;
+            }
+
+
+          }
+        );
+        return name
+      }
+    },
+
     async displayURIName(voc, URI) {
-  
-      let preValues = { name: "", resource: "" };
-
-      let vocMatch =
-        this.voc === "iana-media-types" ||
-        this.voc === "spdx-checksum-algorithm";
       try {
-
-        if (voc !== undefined) {
-          // this is a temporary fix - it should be investiated why country(e.g.) has no vocabulary set!
+        if (voc != undefined) {
           if (voc == "") {
             var arr = URI.split('/');
             voc = arr[arr.length - 2]
+            if (this.property === 'dcatde:politicalGeocodingURI') {
+              let geocodingVoc = ""
+              voc.split(/(?=[A-Z])/).forEach(element => {
+                geocodingVoc += '-' + element.toLowerCase()
+              });
+              voc = 'political-geocoding' + geocodingVoc
+              console.log(voc);
+            }
           }
-
-          // Request is not working properly anymore - ToDo 
-
-          // await this.requestResourceName({ voc: voc, resource: URI }).then(
-          //   (response) => {
-          //     let result = vocMatch
-          //       ? response.data.result.results
-          //         .filter((dataset) => dataset.resource === URI)
-          //         .map((dataset) => dataset.pref_label)[0].en
-          //       : getTranslationFor(response.data.result.pref_label, this.$i18n.locale, []);
-          //     preValues.name = result;
-          //     preValues.resource = URI;
-
-          //   }
-          // );
-          // console.log(preValues);
           if (this.value.type === "multiURI") {
 
-            // New function to gather values from multiURIs -ToDo return to the requestResourceName function because this way
-            this.namesOfMulti = this.data[this.property];
-            // console.log(this.namesOfMulti);
-            // 
+            let resolvedURI = { name: await this.getUriName(voc, URI), resource: URI }
 
-            // this.namesOfMulti.push(preValues.name)
+            if (this.namesOfMulti.find(({ name }) => name === resolvedURI.name) === undefined) {
+              if (resolvedURI.name != undefined) {
+                this.namesOfMulti.push(resolvedURI)
+              } 
+            }
           }
 
-          // this.nameOfProperty = preValues.name
-          this.nameOfProperty = this.data[this.property][0]['name']
-          return preValues;
+          this.nameOfProperty = await this.getUriName(voc, URI)
         }
-        else {
-          this.nameOfProperty = this.data[this.property][0]['name']
-          return
-        }
+        // else {
+        //   this.nameOfProperty = this.data[this.property][0]['name']
+        //   return
+        // }
       } catch (error) {
-        // console.log(error);
-        this.nameOfProperty = URI
+        this.nameOfProperty = await this.getUriName(voc, URI)
       }
 
     }
