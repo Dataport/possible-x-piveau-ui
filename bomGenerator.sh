69403d263f362e349cfc2aec556d0c8f1b8c50a6
msg <majid.salehi.ghamsari@fokus.fraunhofer.de>
2024-05-16 12:41:46 +0200

    fixed create sbom from piveau-hub-ui-modules

--- bomGenerator.sh
+++ bomGenerator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd packages\piveau-hub-ui-modules\
-cyclonedx-npm --output-format JSON --output-file ..\..\bom.json
-cd ..\..
+cd packages/piveau-hub-ui-modules/
+cyclonedx-npm --output-format JSON --output-file ../../bom.json
+cd ../..
