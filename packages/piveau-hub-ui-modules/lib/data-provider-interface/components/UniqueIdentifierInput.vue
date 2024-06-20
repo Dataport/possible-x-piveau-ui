15853c3bc9e286ada7b29a56f67952dad484f4c7
mad1 <Nicolas.Matthaes@fokus.fraunhofer.de>
2024-06-20 15:15:57 +0200

    fix: use env hubUrl value

--- packages/piveau-hub-ui-modules/lib/data-provider-interface/components/UniqueIdentifierInput.vue
+++ packages/piveau-hub-ui-modules/lib/data-provider-interface/components/UniqueIdentifierInput.vue
@@ -3,9 +3,11 @@ import { ref, onMounted, watchEffect } from 'vue'
 import { isNil } from 'lodash';
 import axios from 'axios';
 import { useStore } from 'vuex';
+import { getCurrentInstance } from "vue";
+
+let env = getCurrentInstance().appContext.app.config.globalProperties.$env;
 
 const store = useStore();
-// const isEditMode = store.getters['auth/getIsEditMode'];
 const isEditMode = ref();
 isEditMode.value = store.getters['auth/getIsEditMode'];
 
@@ -16,24 +18,25 @@ const validationMessages = {
 
 async function idunique(node) {
   let isUniqueID = true;
+  let hubUrl = env.api.hubUrl;
   const draftIDs = store.getters['auth/getUserDraftIds'];
 
   new Promise(() => {
     if (isNil(node.value) || node.value === '' || node.value === undefined) isUniqueID = true;
     else if (draftIDs.includes(node.value)) isUniqueID = false;
     else {
-      // TODO: insert env hubUrl
-      const request = `https://piveau-hub-repo-piveau.apps.osc.fokus.fraunhofer.de/datasets/${node.value}?useNormalizedId=true`;
+      const request = `${hubUrl}datasets/${node.value}?useNormalizedId=true`;
       axios.head(request)
         .then(() => {
           isUniqueID = false;
         })
         .catch((e) => {
+          console.log(e)
           isUniqueID = true;
         });
     }
   });
-  return isUniqueID
+  return isUniqueID;
 }
 
 function idformatvalid(node) {
