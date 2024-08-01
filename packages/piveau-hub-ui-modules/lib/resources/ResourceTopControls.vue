da4b146dcada811f19aa1de3a4f7952d345a277a
mad1 <Nicolas.Matthaes@fokus.fraunhofer.de>
2024-08-01 17:26:31 +0200

    feat: Migrate ResourceTopControls.vue to Vue 3 Composition API

--- packages/piveau-hub-ui-modules/lib/resources/ResourceTopControls.vue
+++ packages/piveau-hub-ui-modules/lib/resources/ResourceTopControls.vue
@@ -29,67 +29,70 @@
     </sub-navigation>
   </div>
 </template>
-  
-<script>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+import { useRoute } from 'vue-router';
 import { useResourcesStore } from '../store/resourcesStore';
+
+import { useRuntimeEnv } from '../composables/useRuntimeEnv';
 
 import AppLink from "../widgets/AppLink.vue";
 
-export default {
-  name: "ResourceTopControls",
-  components: {
-    AppLink
-  },
-  data() {
-    return {
-      useFeed: this.$env.content.datasets.useFeed,
-      baseUrl: this.$env.api.baseUrl,
-    }
-  },
-  computed: {
-    getSelectedResource() {
-      return this.resourcesStore.getters.getSelectedResource;
-    },
-    getLimit() {
-      return parseInt(this.$route.query.limit || this.resourcesStore.getters.getLimit);
-    },
-    getPage() {
-      return parseInt(this.$route.query.page || this.resourcesStore.getters.getPage);
-    },
-    getFacets() {
-      return this.resourcesStore.getters.getFacets;
-    },
-    getSort() {
-      return this.resourcesStore.getters.getSort;
-    },
-    showFeed() {
-      return this.useFeed && this.getSelectedResource === 'datasets';
-    },
-  },
-  methods: {
-    getFeedLink(format) {
-      return `${this.baseUrl}${this.$route.query.locale}/feeds/datasets.${format}`;
-    },
-    getFeedQuery() {
-      const feedQuery = {};
-      const query = this.$route?.query
-      if (query?.query) feedQuery.q = query.query;
-      if (this.facetsNotEmpty() && JSON.stringify(this.getFacets)) feedQuery.facets = JSON.stringify(this.getFacets);
-      if (this.getPage) feedQuery.page = Math.max(this.getPage - 1, 0);
-      if (this.getLimit) feedQuery.limit = this.getLimit;
-      feedQuery.facetOperator = query?.facetOperator || 'AND';
-      feedQuery.facetGroupOperator = query?.facetOperator || 'AND';
-      feedQuery.dataServices = query?.dataServices || 'false';
-      if (this.getSort) feedQuery.sort = this.getSort;
-      return feedQuery;
-    },
-    facetsNotEmpty() {
-      return Object.values(this.getFacets).some(facet => facet.length > 0);
-    }
-  },
-  setup() {
-    const resourcesStore = useResourcesStore();
-    return { resourcesStore };
-  },
-}
-</script>  
+const route = useRoute();
+const resourcesStore = useResourcesStore();
+
+const ENV = useRuntimeEnv();
+
+const useFeed = ref(ENV.content.datasets.useFeed);
+const baseUrl = ref(ENV.api.baseUrl);
+
+
+function getFeedLink(format: string) {
+  return `${baseUrl}${route.query.locale}/feeds/datasets.${format}`;
+};
+
+function getFeedQuery() {
+  const feedQuery: any = {};
+  const query = route?.query
+  if (query?.query) feedQuery.q = query.query;
+  if (facetsNotEmpty() && JSON.stringify(getFacets)) feedQuery.facets = JSON.stringify(getFacets);
+  if (getPage) feedQuery.page = Math.max(Number(getPage) - 1, 0);
+  if (getLimit) feedQuery.limit = getLimit;
+  feedQuery.facetOperator = query?.facetOperator || 'AND';
+  feedQuery.facetGroupOperator = query?.facetOperator || 'AND';
+  feedQuery.dataServices = query?.dataServices || 'false';
+  if (getSort) feedQuery.sort = getSort;
+  return feedQuery;
+};
+
+function facetsNotEmpty() {
+  return Object.values(getFacets).some(facet => facet.length > 0);
+};
+
+const getSelectedResource = computed(() => {
+  return resourcesStore.getters.getSelectedResource;
+});
+
+const getLimit = computed(() => {
+  return Number(route.query?.limit || resourcesStore.getters.getLimit);
+});
+
+const getPage = computed(() => {
+  return Number(route.query.page || resourcesStore.getters.getPage);
+});
+
+const getFacets = computed(() => {
+  return resourcesStore.getters.getFacets;
+});
+
+const getSort = computed(() => {
+  return resourcesStore.getters.getSort;
+});
+
+const showFeed = computed(() => {
+  return useFeed && getSelectedResource.value === 'datasets';
+});
+</script>
+  
