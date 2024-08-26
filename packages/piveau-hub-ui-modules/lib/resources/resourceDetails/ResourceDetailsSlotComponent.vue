2edc3b5b8db5c2ca0a6685f1edd4dda1b3c71a03
ade77772 <adeposi.adeosun@fokus.fraunhofer.de>
2024-08-26 17:23:54 +0200

    Updated sub-nav

--- packages/piveau-hub-ui-modules/lib/resources/resourceDetails/ResourceDetailsSlotComponent.vue
+++ packages/piveau-hub-ui-modules/lib/resources/resourceDetails/ResourceDetailsSlotComponent.vue
@@ -2,8 +2,9 @@
   <div class="content-wrapper">
     <span class="resource_banner">{{ $t(`message.header.navigation.data.${selectedResource}`) }}</span>
     <div class="sub_nav">
-        <a href="">Linked Data &#128279;</a>
-        <a href="">Get VC &#x21d3;</a>
+      <a :href="`${hubUrl}resources/${type}/${id}`" target='_blank' class="link">Linked Data &#8599;</a>
+      <a :href="`${hubUrl}trust/${type}/${id}`" target='_blank' class="link">Verifiable Credential &#8599;</a>
+      <a :href="`${hubUrl}trust/${type}/${id}?showCompliance=true`" target='_blank' class="link">Gaia-X Compliance &#8599;</a>
     </div>
     <h1>{{ resourceDetailsData?.name || resourceDetailsData?.id }} </h1>
     <p>{{ resourceDetailsData?.description }}</p>
@@ -13,18 +14,30 @@
 
 <script lang="ts" setup>
 import { defineProps } from 'vue';
+import { useRoute } from 'vue-router';
+import { useRuntimeEnv } from '../../composables/useRuntimeEnv';
+
+const route = useRoute();
+const ENV = useRuntimeEnv();
 
 const props = defineProps({
   selectedResource: String,
   resourceDetailsData: Object,
 });
+
+let type: string = route.params.resource_type.toString();
+let id: string = route.params.resource_id.toString();
+
+const hubUrl = ENV.api.hubUrl;
+
 </script>
 
-<style lang="scss" scoped> 
-.content-wrapper{
+<style lang="scss" scoped>
+.content-wrapper {
   width: 86% !important;
   align-self: center;
 }
+
 .resource_banner {
   background-color: #1E2E4D;
   color: #fff;
@@ -41,9 +54,31 @@ const props = defineProps({
   gap: 1rem;
 
   a {
-    color: #99772C;
+    background-color: #99772C;
+    color: #fff;
+    padding: .5rem .7rem;
+    border-radius: .35rem;
     text-decoration: none;
+    transition-duration: .35s !important; 
+    align-self: flex-start;
+   
+    &:hover{
+      transform: scale(.95);
+      text-decoration: underline
+    }
   }
 
+}
+
+@media screen and (max-width:876px) {
+  .sub_nav{
+    justify-content: flex-start
+  }
+}
+@media screen and (max-width:680px) {
+  .sub_nav{
+    justify-content: flex-start;
+    flex-direction: column
+  }
 }
 </style>
