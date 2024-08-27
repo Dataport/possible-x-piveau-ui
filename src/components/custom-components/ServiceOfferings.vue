133a69277e977ccb959ee1cb1034c360fdbd482f
mad1 <Nicolas.Matthaes@fokus.fraunhofer.de>
2024-08-27 11:30:23 +0200

    fix: Width issue

--- src/components/custom-components/ServiceOfferings.vue
+++ src/components/custom-components/ServiceOfferings.vue
@@ -1,128 +1,126 @@
 <template>
   <div class="page">
-    <div class="content-wrapper">
-      <section class="main_section">
-        <div class="left">
-          <section>
-            <h5>Keywords</h5>
-            <hr />
-            <div class="tag_container">
-              <div class="tag" v-for="(keyword) in resourceDetailsData.keyword">
-                <span>{{ keyword.label }}</span>
-              </div>
+    <section class="main_section">
+      <div class="left">
+        <section>
+          <h5>Keywords</h5>
+          <hr />
+          <div class="tag_container">
+            <div class="tag" v-for="(keyword) in resourceDetailsData.keyword">
+              <span>{{ keyword.label }}</span>
+            </div>
+          </div>
+
+        </section>
+
+        <section>
+          <h5>Service Policy</h5>
+          <hr />
+          <div class="tag_container">
+            <div class="tag" v-for="(policy) in resourceDetailsData.service_policy">
+              <span>{{ policy }}</span>
+            </div>
+          </div>
+
+        </section>
+
+        <section>
+          <h5>Endpoint</h5>
+          <hr />
+          <div class="endpoint">
+            <div>
+              <span class="label">URL:</span>
+              <a href="">{{ resourceDetailsData.endpoint?.endpoint_url }}</a>
             </div>
 
-          </section>
-
-          <section>
-            <h5>Service Policy</h5>
-            <hr />
-            <div class="tag_container">
-              <div class="tag" v-for="(policy) in resourceDetailsData.service_policy">
-                <span>{{ policy }}</span>
-              </div>
+            <div>
+              <span class="label">Formal description:</span>
+              <span>{{ resourceDetailsData.endpoint?.formal_description }}</span>
             </div>
 
-          </section>
-
-          <section>
-            <h5>Endpoint</h5>
-            <hr />
-            <div class="endpoint">
-              <div>
-                <span class="label">URL:</span>
-                <a href="">{{ resourceDetailsData.endpoint?.endpoint_url }}</a>
-              </div>
-
-              <div>
-                <span class="label">Formal description:</span>
-                <span>{{ resourceDetailsData.endpoint?.formal_description }}</span>
-              </div>
-
-              <div>
-                <span class="label">Standard Conformity:</span>
-                <span>
-                  {{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.title }} ({{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.publisher }})
-                </span>
-              </div>
+            <div>
+              <span class="label">Standard Conformity:</span>
+              <span>
+                {{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.title }} ({{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.publisher }})
+              </span>
             </div>
-          </section>
+          </div>
+        </section>
 
+      </div>
+      <div class="right">
+        <div class="info-box">
+          <h5>Terms and Conditions</h5>
+          <hr />
+          <div class="info">
+            <div>
+              <span class="label">URL:</span>
+              <span class="cropped_link"><a href="">{{ resourceDetailsData.terms_and_conditions?.at(0)?.url }}</a></span>
+            </div>
+            <div>
+              <span class="label">Hash:</span>
+              <span>{{ resourceDetailsData.terms_and_conditions?.at(0)?.hash }}</span>
+            </div>
+          </div>
         </div>
-        <div class="right">
-          <div class="info-box">
-            <h5>Terms and Conditions</h5>
-            <hr />
-            <div class="info">
-              <div>
-                <span class="label">URL:</span>
-                <span class="cropped_link"><a href="">{{ resourceDetailsData.terms_and_conditions?.at(0)?.url }}</a></span>
-              </div>
-              <div>
-                <span class="label">Hash:</span>
-                <span>{{ resourceDetailsData.terms_and_conditions?.at(0)?.hash }}</span>
-              </div>
+
+        <div class="info-box">
+          <h5>Data Account Export</h5>
+          <hr />
+          <div class="info">
+            <div>
+              <span class="label">Request Type:</span>
+              <span>{{ resourceDetailsData.data_account_export?.at(0)?.request_type }}</span>
+            </div>
+            <div>
+              <span class="label">Format Type:</span>
+              <span>{{ resourceDetailsData.data_account_export?.at(0)?.format_type }}</span>
+            </div>
+            <div>
+              <span class="label">Access Type:</span>
+              <span>{{ resourceDetailsData.data_account_export?.at(0)?.access_type }}</span>
             </div>
           </div>
-
-          <div class="info-box">
-            <h5>Data Account Export</h5>
-            <hr />
-            <div class="info">
-              <div>
-                <span class="label">Request Type:</span>
-                <span>{{ resourceDetailsData.data_account_export?.at(0)?.request_type }}</span>
-              </div>
-              <div>
-                <span class="label">Format Type:</span>
-                <span>{{ resourceDetailsData.data_account_export?.at(0)?.format_type }}</span>
-              </div>
-              <div>
-                <span class="label">Access Type:</span>
-                <span>{{ resourceDetailsData.data_account_export?.at(0)?.access_type }}</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="info-box">
-            <h5>Hosted On:</h5>
-            <hr />
-            <div class="info">
-              <div>
-                <span class="label">Name:</span>
-                <span>{{ resourceDetailsData.hosted_on?.at(0)?.name }}</span>
-              </div>
-              <div>
-                <span class="label">Description:</span>
-                <span>{{ resourceDetailsData.hosted_on?.at(0)?.description }}</span>
-              </div>
-              <div>
-                <span class="label">Resource Policy:</span>
-                <span>{{ resourceDetailsData.hosted_on?.at(0)?.resource_policy?.at(0) }}</span>
-              </div>
-              <div>
-                <span class="label">License:</span>
-                <span>{{ resourceDetailsData.hosted_on?.at(0)?.license?.at(0) }}</span>
-              </div>
-              <div>
-                <span class="label">Copyright Owned By</span>
-                <span>{{ resourceDetailsData.hosted_on?.at(0)?.copyright_owned_by?.at(0) }}</span>
-              </div>
-            </div>
-          </div>
-
-          <!-- <div class="info-box">
-            <h5>Service Policy: </h5>
-            <hr />
-            <div class="info">
-              <div class="policy_tag" v-for="(policy) in resourceDetailsData.result.service_policy">
-                <span class="label">{{ policy }}</span>
-              </div>
-            </div>
-          </div> -->
         </div>
-      </section>
-    </div>
+
+        <div class="info-box">
+          <h5>Hosted On:</h5>
+          <hr />
+          <div class="info">
+            <div>
+              <span class="label">Name:</span>
+              <span>{{ resourceDetailsData.hosted_on?.at(0)?.name }}</span>
+            </div>
+            <div>
+              <span class="label">Description:</span>
+              <span>{{ resourceDetailsData.hosted_on?.at(0)?.description }}</span>
+            </div>
+            <div>
+              <span class="label">Resource Policy:</span>
+              <span>{{ resourceDetailsData.hosted_on?.at(0)?.resource_policy?.at(0) }}</span>
+            </div>
+            <div>
+              <span class="label">License:</span>
+              <span>{{ resourceDetailsData.hosted_on?.at(0)?.license?.at(0) }}</span>
+            </div>
+            <div>
+              <span class="label">Copyright Owned By</span>
+              <span>{{ resourceDetailsData.hosted_on?.at(0)?.copyright_owned_by?.at(0) }}</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="info-box">
+          <h5>Service Policy: </h5>
+          <hr />
+          <div class="info">
+            <div class="policy_tag" v-for="(policy) in resourceDetailsData.result.service_policy">
+              <span class="label">{{ policy }}</span>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </section>
   </div>
 </template>
 
@@ -136,14 +134,6 @@ const props = defineProps({
 <style lang="scss" scoped>
 .page {
   width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-
-  .content-wrapper {
-    width: 73%;
-  }
 }
 
 a {
