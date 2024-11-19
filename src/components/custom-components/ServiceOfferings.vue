cc959c267ce0e73e649f424a11c8b9221de2bba7
mic43145 <michael.gysel@fokus.fraunhofer.de>
2024-11-19 15:11:12 +0100

    Updated Copy ID Button styling

--- src/components/custom-components/ServiceOfferings.vue
+++ src/components/custom-components/ServiceOfferings.vue
@@ -1,150 +1,6 @@
 <template>
   <div class="page">
     <section class="main_section pt-5">
-      <!-- <div class="left">
-        <section>
-          <h4>Keywords</h4>
-          <hr />
-          <div class="tag_container">
-            <div class="tag" v-for="(keyword) in resourceDetailsData.keyword" v-bind:key="keyword">
-              <span>{{ keyword.label }}</span>
-            </div>
-          </div>
-
-        </section>
-
-        <section>
-          <h4>Service Policy</h4>
-          <hr />
-          <div class="tag_container">
-            <div class="tag" v-for="(policy) in resourceDetailsData.service_policy" v-bind:key="policy">
-              <span>{{ policy }}</span>
-            </div>
-          </div>
-
-        </section>
-
-        <section>
-          <h4>Endpoint</h4>
-          <hr />
-          <div class="endpoint">
-            <div>
-              <span class="label">URL:</span>
-              <a v-bind:href=resourceDetailsData.endpoint?.endpoint_url target="_blank">{{ resourceDetailsData.endpoint?.endpoint_url }}</a>
-            </div>
-
-            <div>
-              <span class="label">Formal description:</span>
-              <span>{{ resourceDetailsData.endpoint?.formal_description }}</span>
-            </div>
-
-            <div>
-              <span class="label">Standard Conformity:</span>
-              <span>
-                {{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.title }} ({{ resourceDetailsData.endpoint?.standard_conformity?.at(0)?.publisher }})
-              </span>
-            </div>
-          </div>
-        </section>
-
-      </div>
-      <div class="right">
-          <div class="info-box">
-          <h5>General</h5>
-          <hr />
-          <div class="info">
-            <div>
-              <span class="label">Provision Type:</span>
-             <span>{{ resourceDetailsData.provision_type }}</span>
-            </div>
-            <div>
-              <span class="label">Data Protection Regime:</span>
-              <span>{{ resourceDetailsData.data_protection_regime?.at(0) }}</span>
-            </div>
-          </div>
-        </div>
-        <div class="info-box">
-          <h5>Terms and Conditions</h5>
-          <hr />
-          <div class="info">
-            <div>
-              <span class="label">URL:</span>
-              <span class="cropped_link"><a v-bind:href=resourceDetailsData.terms_and_conditions?.at(0)?.url target="_blank">{{ resourceDetailsData.terms_and_conditions?.at(0)?.url }}</a></span>
-            </div>
-            <div>
-              <span class="label">Hash:</span>
-              <span>{{ resourceDetailsData.terms_and_conditions?.at(0)?.hash }}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="info-box">
-          <h5>Data Account Export</h5>
-          <hr />
-          <div class="info">
-            <div>
-              <span class="label">Request Type:</span>
-              <span>{{ resourceDetailsData.data_account_export?.at(0)?.request_type }}</span>
-            </div>
-            <div>
-              <span class="label">Format Type:</span>
-              <span>{{ resourceDetailsData.data_account_export?.at(0)?.format_type }}</span>
-            </div>
-            <div>
-              <span class="label">Access Type:</span>
-              <span>{{ resourceDetailsData.data_account_export?.at(0)?.access_type }}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="info-box">
-          <h5>Hosted On:</h5>
-          <hr />
-          <div class="info">
-            <div>
-              <span class="label">Name:</span>
-              <span>{{ resourceDetailsData.hosted_on?.at(0)?.name }}</span>
-            </div>
-            <div>
-              <span class="label">Description:</span>
-              <span>{{ resourceDetailsData.hosted_on?.at(0)?.description }}</span>
-            </div>
-            <div>
-              <span class="label">Resource Policy:</span>
-              <span>{{ resourceDetailsData.hosted_on?.at(0)?.resource_policy?.at(0) }}</span>
-            </div>
-            <div>
-              <span class="label">License:</span>
-              <span>{{ resourceDetailsData.hosted_on?.at(0)?.license?.at(0) }}</span>
-            </div>
-            <div>
-              <span class="label">Copyright Owned By</span>
-              <span>{{ resourceDetailsData.hosted_on?.at(0)?.copyright_owned_by?.at(0) }}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="info-box">
-          <h5>Service Policy: </h5>
-          <hr />
-          <div class="info">
-            <div class="policy_tag" v-for="(policy) in resourceDetailsData.result.service_policy">
-              <span class="label">{{ policy }}</span>
-            </div>
-          </div>
-        </div>
-      </div>  -->
-      <!-- <section>
-          <h4>Keywords</h4>
-          <hr />
-          <div class="tag_container">
-            <div class="tag" v-for="(keyword) in resourceDetailsData.keyword" v-bind:key="keyword">
-              <span>{{ keyword.label }}</span>
-            </div>
-          </div>
-
-        </section> -->
-      
       <section>
         <h4>Service Offering ID</h4>
         <hr />
@@ -435,7 +291,6 @@ a {
   padding-bottom: 4px;
 }
 
-
 .copy-button {
   background-color: #314D84;
   color: #fff;
@@ -443,10 +298,12 @@ a {
   padding: 0.4rem 0.6rem;
   border-radius: 0.3rem;
   cursor: pointer;
-  transition: background-color 0.3s;
+  transition: background-color 0.3s, transform 0.3s, text-decoration 0.3s;
 
   &:hover {
     background-color: #2A3F6E;
+    text-decoration: underline; /* Underline text on hover */
+    transform: scale(0.95); /* Slightly shrink the button */
   }
 }
 
