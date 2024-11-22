925614869566fecb12c4253a6f7ad73247d47017
mic43145 <michael.gysel@fokus.fraunhofer.de>
2024-11-22 10:29:04 +0100

    Updated monorepo to remove 'click me' button

--- src/components/custom-components/DataProducts.vue
+++ src/components/custom-components/DataProducts.vue
@@ -8,16 +8,23 @@
         <hr />
         <div class="id-container">
           <span class="id-text">{{ resourceDetailsData.id }}</span>
-          <button class="copy-button" @click="copyToClipboard(resourceDetailsData.id)">
+          <button class="copy-button" @click="copyId">
             Copy ID
           </button>
         </div>
+        <div v-if="notificationVisible" class="notification">
+          Data Product ID Copied
+        </div>
       </section>
 
-      <section class="">
+      <section>
         <h4>Copyright Owned By:</h4>
         <hr />
-        <div class="tag" v-for="(cob) in resourceDetailsData.aggregation_of?.[0]?.copyright_owned_by" v-bind:key="cob">
+        <div
+          class="tag"
+          v-for="(cob, index) in resourceDetailsData.aggregation_of?.[0]?.copyright_owned_by"
+          :key="index"
+        >
           <span>{{ cob }}</span>
         </div>
       </section>
@@ -26,29 +33,35 @@
         <h4>License</h4>
         <hr />
         <div class="tag_container">
-          <div class="tag" v-for="(license) in resourceDetailsData.aggregation_of?.at(0)?.license" v-bind:key="license">
+          <div
+            class="tag"
+            v-for="(license, index) in resourceDetailsData.aggregation_of?.[0]?.license"
+            :key="index"
+          >
             <span>{{ license }}</span>
           </div>
         </div>
       </section>
 
-
       <section>
         <h4>Produced by</h4>
         <hr />
-        <p>{{ resourceDetailsData.aggregation_of?.at(0)?.produced_by }}</p>
+        <p>{{ resourceDetailsData.aggregation_of?.[0]?.produced_by }}</p>
       </section>
 
       <section>
         <h4>Data Resource Policy</h4>
         <hr />
         <div class="tag_container">
-          <div class="tag" v-for="(policy) in resourceDetailsData.aggregation_of?.at(0)?.policy" v-bind:key="policy">
+          <div
+            class="tag"
+            v-for="(policy, index) in resourceDetailsData.aggregation_of?.[0]?.policy"
+            :key="index"
+          >
             <span>{{ policy }}</span>
           </div>
         </div>
       </section>
-
 
       <section class="additional_info">
         <h3>Associated Service Offering</h3>
@@ -79,10 +92,16 @@
           <h4>Data Protection Regime:</h4>
           <div class="tag_container">
             <div
-              v-if="!resourceDetailsData.data_protection_regime || resourceDetailsData.data_protection_regime.length === 0">
+              v-if="!resourceDetailsData.data_protection_regime || resourceDetailsData.data_protection_regime.length === 0"
+            >
               <span>None</span>
             </div>
-            <div class="tag" v-else v-for="(regime, index) in resourceDetailsData.data_protection_regime" :key="index">
+            <div
+              class="tag"
+              v-else
+              v-for="(regime, index) in resourceDetailsData.data_protection_regime"
+              :key="index"
+            >
               <span>{{ regime }}</span>
             </div>
           </div>
@@ -90,14 +109,13 @@
 
         <hr />
 
-        <section class="">
+        <section>
           <h4>Data Account Export:</h4>
-
           <div class="tag_container">
-            <div class="" v-for="(account_export) in resourceDetailsData.data_account_export"
-              v-bind:key="data_account_export">
-
-
+            <div
+              v-for="(account_export, index) in resourceDetailsData.data_account_export"
+              :key="index"
+            >
               <div class="tag">
                 <div>
                   <span class="label">Format Type:</span>
@@ -114,48 +132,71 @@
               </div>
             </div>
           </div>
-
-
         </section>
-
 
         <hr />
 
-        <section class="">
+        <section>
           <h4>Terms and Conditions:</h4>
-
           <div class="tag_container">
-            <div class="" v-for="(t_c) in resourceDetailsData.terms_and_conditions" v-bind:key="terms_and_conditions">
-
-
+            <div
+              v-for="(t_c, index) in resourceDetailsData.terms_and_conditions"
+              :key="index"
+            >
               <div class="tag">
                 <div>
                   <span class="label">URL:</span>
-                  <span><a v-bind:href="`${t_c.url}`">{{ t_c.url }}</a></span>
+                  <span><a :href="t_c.url">{{ t_c.url }}</a></span>
                 </div>
                 <div>
                   <span class="label">Hash:</span>
                   <span>{{ t_c.hash }}</span>
                 </div>
-
               </div>
             </div>
           </div>
         </section>
       </section>
-
     </section>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
   selectedResource: String,
   resourceDetailsData: Object,
-  copyToClipboard:Function
+  copyToClipboard: Function,
 });
 
+const notificationVisible = ref(false);
 
+function copyId() {
+  if (props.copyToClipboard) {
+    props.copyToClipboard(props.resourceDetailsData.id);
+  } else if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard.writeText(props.resourceDetailsData.id);
+  } else {
+    // Fallback for older browsers
+    const textarea = document.createElement('textarea');
+    textarea.value = props.resourceDetailsData.id;
+    textarea.style.position = 'fixed'; // Avoid scrolling to bottom
+    document.body.appendChild(textarea);
+    textarea.focus();
+    textarea.select();
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error('Fallback: Unable to copy', err);
+    }
+    document.body.removeChild(textarea);
+  }
+  notificationVisible.value = true;
+  setTimeout(() => {
+    notificationVisible.value = false;
+  }, 3000); // Hide notification after 3 seconds
+}
 
 console.log('Resource Details Data:', props.resourceDetailsData);
 </script>
@@ -166,21 +207,13 @@ console.log('Resource Details Data:', props.resourceDetailsData);
 }
 
 a {
-  text-decoration: underline
+  text-decoration: underline;
 }
 
 .cropped_link {
-
-  -ms-word-break: break-all;
   word-break: break-all;
-  /* Non standard for webkit */
   word-break: break-word;
-
-  -webkit-hyphens: auto;
-  -moz-hyphens: auto;
-  -ms-hyphens: auto;
   hyphens: auto;
-
 }
 
 .main_section {
@@ -205,7 +238,7 @@ a {
 }
 
 .copy-button {
-  background-color: #314D84;
+  background-color: #314d84;
   color: #fff;
   border: none;
   padding: 0.4rem 0.6rem;
@@ -214,7 +247,7 @@ a {
   transition: background-color 0.3s, transform 0.3s, text-decoration 0.3s;
 
   &:hover {
-    background-color: #2A3F6E;
+    background-color: #2a3f6e;
     text-decoration: underline;
     transform: scale(0.95);
   }
@@ -223,21 +256,16 @@ a {
 .tag_container {
   display: flex !important;
   flex-wrap: wrap;
-  gap: .5rem;
-
-
-
+  gap: 0.5rem;
 
   .tag {
-
-    background-color: #314D84;
+    background-color: #314d84;
     color: #fff;
-    padding: .4rem .6rem;
-    border-radius: .3rem;
+    padding: 0.4rem 0.6rem;
+    border-radius: 0.3rem;
 
     a {
       color: #fff !important;
-
     }
   }
 }
@@ -247,17 +275,18 @@ a {
 }
 
 .info-box {
-  background-color: #1E2E4D;
+  background-color: #1e2e4d;
   color: #fff;
-  padding: .0rem 0rem;
-  border-radius: .6rem;
+  padding: 0rem 0rem;
+  border-radius: 0.6rem;
 
   .info {
     display: flex;
     flex-direction: column;
-    gap: .5rem;
+    gap: 0.5rem;
 
-    div {}
+    div {
+    }
   }
 
   a {
@@ -277,13 +306,12 @@ a {
 .right {
   h4 {
     border: 1px solid #fff;
-    border-top-left-radius: .6rem;
-    border-top-right-radius: .6rem;
-
+    border-top-left-radius: 0.6rem;
+    border-top-right-radius: 0.6rem;
   }
 
   .info {
-    padding: 1rem
+    padding: 1rem;
   }
 }
 
@@ -299,25 +327,23 @@ a {
 
 .policy_tag {
   .label {
-    background-color: #D99809;
+    background-color: #d99809;
     color: #fff;
-    padding: .4rem .6rem;
-    border-radius: .3rem;
-
+    padding: 0.4rem 0.6rem;
+    border-radius: 0.3rem;
   }
 }
 
 @media screen and (max-width: 1024px) {
   .page {
     .content-wrapper {
-      width: 98%
+      width: 98%;
     }
   }
 }
 
 @media screen and (max-width: 768px) {
   .page {
-
     .main_section {
       flex-direction: column;
 
@@ -331,11 +357,11 @@ a {
 
 .additional_info {
   margin-top: 3rem;
-  -webkit-box-shadow: 1px 7px 15px -8px #E4E4E7;
+  -webkit-box-shadow: 1px 7px 15px -8px #e4e4e7;
   box-shadow: 1px 7px 15px -8px #e4e4e7;
   border: 2px solid #e4e4e7;
   padding: 1rem;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
   width: 100% !important;
 
   h4 {
@@ -348,21 +374,25 @@ a {
   }
 
   .description_tag {
-    margin-top: .2rem;
+    margin-top: 0.2rem;
   }
 
   section {
     display: flex;
     flex-direction: column;
-    gap: .5rem;
-
-
+    gap: 0.5rem;
   }
-
-
 
   h3 {
-    text-align: center
+    text-align: center;
   }
+}
+
+.notification {
+  background-color: #d4edda; /* light green background */
+  color: #155724; /* dark green text */
+  padding: 1rem;
+  border-radius: 0.25rem;
+  margin-top: 1rem;
 }
 </style>
